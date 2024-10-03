ebe66235cd30818d1c37a4ec7cfaecdfa1e3a67e
Thomas Tam <tyttam@gmail.com>
2024-10-03 17:10:08 -0400

    Sum Digit  Challenge

---
+++ DigitSumChallenge/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("The sum of the digits in number 1234 is " + sumDigits(1234));
+        System.out.println("The sum of the digits in number -125 is " + sumDigits(-125));
+        System.out.println("The sum of the digits in number 4 is " + sumDigits(4));
+        System.out.println("The sum of the digits in number 32123 is " + sumDigits(32123));
+
+    }
+
+    public static int sumDigits (int number) {
+        int sum = 0;
+
+        if (number < 0){
+            return -1;
+        }
+        while (number > 0) {
+            sum += number % 10; // sum the last digit
+            number /= 10; // remove the last digit
+        }
+        return sum;
+    }
+}
