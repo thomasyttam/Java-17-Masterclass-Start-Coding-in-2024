a26bfc48d580e906e54e5a475d1c8dec62e1a47e
Thomas Tam <tyttam@gmail.com>
2024-10-25 14:24:08 -0400

    Classes - getter and setter

--- ClassesPart1/src/Car.java
+++ ClassesPart1/src/Car.java
@@ -26,6 +26,33 @@ public class Car {
         return conventible;
     }
 
+    public void setMake (String make){
+        if (make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake){
+            case "holden", "porsche", "tesla" -> this.make = make;
+            default -> {
+                this.make = "Unsupported";
+            }
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConventible(boolean conventible) {
+        this.conventible = conventible;
+    }
+
     public void describeCar() {
         System.out.println(doors + "-Door " +
                 color + " " +
--- ClassesPart1/src/Main.java
+++ ClassesPart1/src/Main.java
@@ -2,11 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
         Car car = new Car();
-//        car.make = "Porche";
-//        car.model = "Carrera";
-//        car.color = "red";
+        car.setMake("Porsche");
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setConventible(true);
+        car.setColor("black");
         System.out.println("make = " + car.getMake());
         System.out.println("model  = " + car.getModel());
         car.describeCar();
+
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        targa.setConventible(false);
+        targa.setColor("red");
+        targa.describeCar();
+
     }
 }
