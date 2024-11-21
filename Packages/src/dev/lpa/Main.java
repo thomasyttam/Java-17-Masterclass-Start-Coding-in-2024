2e9b9ffb2e64e1d36db4525b2cc4c089e5a07489
Thomas Tam <tyttam@gmail.com>
2024-11-21 15:32:01 -0500

    First Package

---
+++ Packages/src/dev/lpa/Main.java
@@ -0,0 +1,12 @@
+package dev.lpa;
+
+import com.abc.first.Item;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
+}
