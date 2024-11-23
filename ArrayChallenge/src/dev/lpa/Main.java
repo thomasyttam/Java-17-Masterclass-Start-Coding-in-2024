b575ae504c86f3676f094cbd1eaddbbdf0992960
Thomas Tam <tyttam@gmail.com>
2024-11-23 10:53:40 -0500

    Array Challenge - init

---
+++ ArrayChallenge/src/dev/lpa/Main.java
@@ -0,0 +1,15 @@
+package dev.lpa;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] getRandomIntArray{
+        Random random = new Random();
+        
+    }
+}
