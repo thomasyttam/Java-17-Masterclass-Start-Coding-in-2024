109cb66e10489d186ac5881500602982408c2afc
Thomas Tam <tyttam@gmail.com>
2024-12-02 21:07:55 -0500

    Multiple Arrays - 2

--- MultipleArrays/src/dev/lpa/Main.java
+++ MultipleArrays/src/dev/lpa/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println(Arrays.toString(array2));
         System.out.println("array2.length = " + array2.length);
 
-        for(int[] outer : array2){
+        for (int[] outer : array2) {
             System.out.println(outer);
             System.out.println(Arrays.toString(outer));
         }
@@ -24,13 +24,40 @@ public class Main {
             //System.out.println();
         }
 
-        for(var outer : array2){
-            for(var element : outer){
+        for (var outer : array2) {
+            for (var element : outer) {
                 System.out.print(element + " ");
             }
             System.out.println();
         }
 
         System.out.println(Arrays.deepToString(array2));
+
+        // array2[1] = {10, 20, 30} --> cannot use array initializer
+        array2[1] = new int[]{10, 20, 30};
+        System.out.println(Arrays.deepToString(array2));
+
+        Object[] anyArray = new Object[3];
+        System.out.println(Arrays.toString(anyArray));
+
+        anyArray[0] = new String[]{"a", "b", "c"};
+        System.out.println(Arrays.deepToString(anyArray));
+
+        anyArray[1] = new String [][]{
+                {"1", "2"},
+                {"3", "4", "5"},
+                {"6", "7", "8", "9"}
+        };
+        System.out.println(Arrays.deepToString(anyArray));
+
+        // anyArray[2] = "Hello"; -> cannot convert String in class
+        anyArray[2] = new int[2][2][2];
+        System.out.println(Arrays.deepToString(anyArray));
+
+        for(Object element : anyArray){
+            System.out.println("Element type = " + element.getClass().getSimpleName());
+            System.out.println("Element toString() = " + element);
+            System.out.println(Arrays.deepToString((Object[]) element));
+        }
     }
 }
