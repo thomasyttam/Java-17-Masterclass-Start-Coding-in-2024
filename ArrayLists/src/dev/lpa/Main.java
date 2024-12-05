04f101f7239dded332d69947303301fa9b60929c
Thomas Tam <tyttam@gmail.com>
2024-12-05 17:25:01 -0500

    Array Lists

--- ArrayLists/src/dev/lpa/Main.java
+++ ArrayLists/src/dev/lpa/Main.java
@@ -8,6 +8,11 @@ record GroceryItem(String name, String type, int count) {
     public GroceryItem(String name){
         this(name, "DAIRY", 1);
     }
+
+    @Override
+    public String toString(){
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
 }
 
 public class Main {
@@ -22,12 +27,18 @@ public class Main {
 //        System.out.println(Arrays.toString(groceryArray));
 
 
+        // Array
+        System.out.println("Array");
+        System.out.println("-".repeat(20));
         GroceryItem[] groceryArray = new GroceryItem[3];
         groceryArray[0] = new GroceryItem("milk");
         groceryArray[1] = new GroceryItem("apple", "PRODUCE", 6);
         groceryArray[2] = new GroceryItem("orange","PRODUCE", 5);
         System.out.println(Arrays.toString(groceryArray));
+        System.out.println("-".repeat(20));
 
+        System.out.println("Array List");
+        System.out.println("-".repeat(20));
         // General list can add any type
         ArrayList objectList = new ArrayList();
         objectList.add(new GroceryItem("Butter"));
@@ -36,5 +47,16 @@ public class Main {
         // Specify the type in the array list
         ArrayList<GroceryItem> groceryList = new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
+        // groceryList.add("Yogurt"); -> cannot work, need GroceryItem
+        groceryList.add(new GroceryItem("Milk"));
+        groceryList.add(new GroceryItem("Oranges", "PRODUCE", 5));
+        groceryList.add(0,
+                new GroceryItem("Apples", "PRODUCE", 6));
+        groceryList.set(0,
+                new GroceryItem("Apples", "PRODUCE", 6));
+        System.out.println(groceryList);
+        groceryList.remove(1); // remove the item in index 1
+        System.out.println(groceryList);
+
     }
 }
---
+++ ArrayLists/src/dev/lpa/MoreLists.java
@@ -0,0 +1,8 @@
+package dev.lpa;
+
+public class MoreLists {
+
+    public static void main(String[] args) {
+        
+    }
+}
