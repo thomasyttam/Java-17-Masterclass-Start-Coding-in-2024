7f8ebd81d4a2e190a82a5c26dcde5ebcbadf053d
Thomas Tam <tyttam@gmail.com>
2024-12-07 01:32:18 -0500

    Array and ArrayList

---
+++ ArrayAndArrayLists/src/dev/lpa/Main.java
@@ -0,0 +1,30 @@
+package dev.lpa;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] originalArray = new String[] {"First", "Second", "Third"};
+        var originalList = Arrays.asList(originalArray);
+
+        originalList.set(0, "one");
+        System.out.println("list: " + originalList);
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+        originalList.sort(Comparator.naturalOrder());
+        System.out.println("list: " + originalList);
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+        // Cannot use remove and add as it is from an array, fixed size.
+        // originalList.remove(0);
+        // originalList.add("fourth");
+
+        List<String> newList = Arrays.asList("Sunday", "Monday", "Tuesday");
+        System.out.println(newList);
+
+    }
+}
