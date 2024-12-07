12c39318ac560dc208b5351bcb437130695a58ce
Thomas Tam <tyttam@gmail.com>
2024-12-06 22:48:10 -0500

    Array Lists

--- ArrayLists/src/dev/lpa/MoreLists.java
+++ ArrayLists/src/dev/lpa/MoreLists.java
@@ -22,8 +22,34 @@ public class MoreLists {
                 List.of("pickles", "mustard", "cheese"));
         System.out.println(nextList);
 
+        // addAll add all the item in nextList after the last element of nextList
         groceries.addAll(nextList);
         System.out.println(groceries);
-        
+
+        System.out.println("Third item = " + groceries.get(2));
+
+        if(groceries.contains("mustard")) {
+            System.out.println("List contains mustard");
+        }
+
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("last = " + groceries.lastIndexOf("yogurt"));
+
+        System.out.println(groceries);
+        groceries.remove(1);
+        System.out.println(groceries);
+        groceries.remove("yogurt"); // remove one element
+        System.out.println(groceries);
+
+        groceries.removeAll(List.of("apples", "eggs"));
+        System.out.println(groceries);
+
+        groceries.retainAll(List.of("apples", "milk", "mustard", "cheese"));
+        System.out.println(groceries);
+
+        groceries.clear();
+        System.out.println(groceries);
+        System.out.println("isEmpty = " + groceries.isEmpty());
     }
 }
