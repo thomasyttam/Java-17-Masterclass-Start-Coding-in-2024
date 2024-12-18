5c423db6f860f9fa951dd805f18b73b5b3b9176b
Thomas Tam <tyttam@gmail.com>
2024-12-17 20:07:04 -0500

    LinkedList Challenge - course version

--- LinkedListChallenge/src/dev/lpa/Main.java
+++ LinkedListChallenge/src/dev/lpa/Main.java
@@ -16,9 +16,61 @@ record TownDistance(String town, int distanceFromSyd) {
     }
 }
 
+record Place(String name, int distance){
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", name, distance);
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
+        // Call my Solution for testing
+        // mySolution();
+
+        // Course solution
+        LinkedList<Place> placesToVisit = new LinkedList<>();
+        Place adelaide = new Place("Adelaide", 1374);
+        addPlace(placesToVisit, adelaide);
+        addPlace(placesToVisit, new Place("adelaide", 1374));
+        addPlace(placesToVisit, new Place("Brisbane", 917));
+        addPlace(placesToVisit, new Place("Perth", 3923));
+        addPlace(placesToVisit, new Place("Alice Springs", 2771));
+        addPlace(placesToVisit, new Place("Darwin", 3972));
+        addPlace(placesToVisit, new Place("Melbourne", 877));
+
+        placesToVisit.addFirst(new Place("Sydney", 0));
+        System.out.println(placesToVisit);
+
+    }
+
+    private static void addPlace(LinkedList<Place> list, Place place) {
+        if(list.contains(place)){
+            System.out.println("Found duplicate: " + place);
+            return;
+        }
+
+        for(Place p : list){
+            if(p.name().equalsIgnoreCase(place.name())){
+                System.out.println("Found duplicate: " + place);
+                return;
+            }
+        }
+
+        int matchedIndex = 0;
+        for(var listplace: list) {
+            if(place.distance() < listplace.distance()){
+                list.add(matchedIndex, place);
+                return;
+            }
+            matchedIndex++;
+        }
+        list.add(place);
+    }
+
+    private static void mySolution(){
+
         LinkedList<TownDistance> placeToVisit = new LinkedList<>();
 
         placeToVisit.add(new TownDistance("Sydney", 0));
@@ -119,3 +171,4 @@ public class Main {
         }
     }
 }
+
