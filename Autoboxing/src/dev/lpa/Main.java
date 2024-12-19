b406733cb889d7b207cc278a38185af8ac16d2a2
Thomas Tam <tyttam@gmail.com>
2024-12-19 17:06:22 -0500

    Autoboxing

---
+++ Autoboxing/src/dev/lpa/Main.java
@@ -0,0 +1,17 @@
+package dev.lpa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Integer boxedInt = Integer.valueOf(15);  // preferred but unnecessary
+        Integer deprecatedBoxing = new Integer(15);  // deprecated since JDK9
+        int unboxedInt = boxedInt.intValue();   // unnecessary
+
+        // Automatic
+        Integer autoBoxed = 15;
+        int autoUnboxed = autoBoxed;
+        System.out.println(autoBoxed.getClass().getName());
+//        System.out.println(autoUnboxed.getClass().getName());
+    }
+}
