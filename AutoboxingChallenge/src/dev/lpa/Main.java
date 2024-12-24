3dfdd903cc290f277a53f808ebfda50f593bebd1
Thomas Tam <tyttam@gmail.com>
2024-12-24 09:00:01 -0500

    Autoboxing Challenge - Customer

--- AutoboxingChallenge/src/dev/lpa/Main.java
+++ AutoboxingChallenge/src/dev/lpa/Main.java
@@ -1,8 +1,18 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+
+record Customer(String name, ArrayList<Double> transactions) {
+
+    public Customer(String name, Double initialDeposit) {
+        this(name.toUpperCase(), new ArrayList<Double>(500));
+        transactions.add(initialDeposit);
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 }
