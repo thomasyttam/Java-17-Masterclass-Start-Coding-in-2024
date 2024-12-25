54c6bcb2a78b76213c0e068f91a39a7465c606bd
Thomas Tam <tyttam@gmail.com>
2024-12-25 10:23:53 -0500

    Autoboxing Challenge - Bank

--- AutoboxingChallenge/src/dev/lpa/Main.java
+++ AutoboxingChallenge/src/dev/lpa/Main.java
@@ -14,5 +14,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Customer thomas = new Customer("Thomas", 1000.00);
+        System.out.println(thomas);
+    }
+}
+
+class Bank {
+
+    private String name;
+    private ArrayList<Customer> customers = new ArrayList<>(5000);
+
+    public Bank(String name) {
+        this.name = name;
     }
 }
