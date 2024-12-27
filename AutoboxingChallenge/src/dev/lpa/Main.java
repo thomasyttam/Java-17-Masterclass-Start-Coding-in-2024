fc6b09dbd4c01f688bf5d4700b93ca7830012817
Thomas Tam <tyttam@gmail.com>
2024-12-27 15:15:21 -0500

    Autoboxing Challenge

--- AutoboxingChallenge/src/dev/lpa/Main.java
+++ AutoboxingChallenge/src/dev/lpa/Main.java
@@ -14,8 +14,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Customer thomas = new Customer("Thomas", 1000.00);
+        Customer thomas = new Customer("Thomas T", 1000.00);
         System.out.println(thomas);
+
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("Jane A", 500.0);
+        System.out.println(bank);
+
+        bank.addTransactions("Jane A", -10.25);
+        bank.addTransactions("jane A", -75.01);
+        bank.printStatement("Jane a");
+
+        bank.addNewCustomer("Thomas T", 0);
+        bank.addTransactions("Thomas t", 1000);
+        bank.printStatement("ThomAs T");
+
+
     }
 }
 
@@ -26,6 +40,14 @@ class Bank {
 
     public Bank(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "name='" + name + '\'' +
+                ", customers=" + customers +
+                '}';
     }
 
     private Customer getCustomer(String customerName){
@@ -39,11 +61,32 @@ class Bank {
         return null;
     }
 
-    private void addNewCustomer(String customerName, double initialDeposit) {
+    public void addNewCustomer(String customerName, double initialDeposit) {
         if(getCustomer(customerName) == null){
             Customer customer = new Customer(customerName, initialDeposit);
             customers.add(customer);
             System.out.println("New customer added: " + customer);
+        }
+    }
+
+    public void addTransactions(String name, double transactionAmount) {
+        Customer customer = getCustomer(name);
+        if(customer != null) {
+            customer.transactions().add(transactionAmount);
+        }
+    }
+
+    public void printStatement(String customerName) {
+        Customer customer = getCustomer(customerName);
+        if(customer == null) {
+            return;
+        }
+
+        System.out.println("-".repeat(30));
+        System.out.println("Customer Name: " + customer.name());
+        System.out.println("Transactions:");
+        for (double d : customer.transactions()){
+            System.out.printf("$%10.2f (%s)%n", d, d < 0 ? "debit" : "credit");
         }
     }
 }
