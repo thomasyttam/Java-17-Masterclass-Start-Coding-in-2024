fe4e43dace6acc5cfffa280cb55561d5d575f1ee
Thomas Tam <tyttam@gmail.com>
2025-01-08 18:24:04 -0500

    Interfaces

--- Intefaces/src/dev/lpa/Animal.java
+++ Intefaces/src/dev/lpa/Animal.java
@@ -2,6 +2,9 @@ package dev.lpa;
 
 interface FlightEnabled {
 
+    public static final double MILES_TO_KM = 1.60934;   // public, static, final are redundant
+    double KM_TO_MILES = 0.621371;
+
     public abstract void takeOff(); // public and abstract modifiers are redundant,
                                     // meaning unnecessary to declare
     abstract void land();           // abstract modifier is redundant, meaning
--- Intefaces/src/dev/lpa/Main.java
+++ Intefaces/src/dev/lpa/Main.java
@@ -13,9 +13,33 @@ public class Main {
 //        flier.move();
 //        tracked.move();
 
+//        flier.takeOff();
+//        flier.fly();
+//        tracked.track();
+//        flier.land();
+//        All birds below
+//        System.out.println(animal.getClass().getSimpleName());
+//        System.out.println(flier.getClass().getSimpleName());
+//        System.out.println(tracked.getClass().getSimpleName());
+
+        inFlight(flier);
+        inFlight(new Jet());
+        Trackable truck = new Truck();
+        truck.track();
+
+        double kmsTraveled = 100;
+        double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
+
+        System.out.printf("The truck traveled %.2f km or %.2f miles%n",
+                kmsTraveled, milesTraveled);
+    }
+
+    private static void inFlight(FlightEnabled flier) {
         flier.takeOff();
         flier.fly();
-        tracked.track();
+        if(flier instanceof Trackable tracked) {
+            tracked.track();
+        }
         flier.land();
     }
 }
