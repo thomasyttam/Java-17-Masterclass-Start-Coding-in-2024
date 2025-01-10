178b2a90b6cb915f406290917446bb1d496ada38
Thomas Tam <tyttam@gmail.com>
2025-01-09 20:08:21 -0500

    Interfaces

--- Intefaces/src/dev/lpa/Animal.java
+++ Intefaces/src/dev/lpa/Animal.java
@@ -1,5 +1,33 @@
 package dev.lpa;
 
+
+class Satellite implements OrbitEarth {
+
+    public void achieveOrbit() {
+        System.out.println("Orbit achieved!");
+    };
+
+    @Override
+    public void takeOff() {
+        
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+}
+interface OrbitEarth extends FlightEnabled {
+
+    void achieveOrbit();
+}
+
 interface FlightEnabled {
 
     public static final double MILES_TO_KM = 1.60934;   // public, static, final are redundant
