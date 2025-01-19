00aa5f2c075cbec5bb9462aac4e57525b4cf44cd
Thomas Tam <tyttam@gmail.com>
2025-01-19 13:08:40 -0500

    Interface Challenge

--- InterfaceChallenge/src/dev/lpa/UtilityLine.java
+++ InterfaceChallenge/src/dev/lpa/UtilityLine.java
@@ -1,4 +1,29 @@
 package dev.lpa;
 
-public class UtilityLine {
+enum UtilityType {ELECTRICAL, FIBER_OPTIC, GAS, WATER}
+
+public class UtilityLine implements Mappable{
+
+    private String name;
+    private UtilityType type;
+
+    public UtilityLine(String name, UtilityType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    @Override
+    public String getLabel() {
+        return "";
+    }
+
+    @Override
+    public Geometry getShape() {
+        return null;
+    }
+
+    @Override
+    public String getMarker() {
+        return "";
+    }
 }
