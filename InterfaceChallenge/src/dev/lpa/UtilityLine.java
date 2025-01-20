ec156a9b825dad1bb6f26130ba445788dbc10a0d
Thomas Tam <tyttam@gmail.com>
2025-01-20 07:24:18 -0500

    Interface Challenge

--- InterfaceChallenge/src/dev/lpa/UtilityLine.java
+++ InterfaceChallenge/src/dev/lpa/UtilityLine.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.awt.*;
+
 enum UtilityType {ELECTRICAL, FIBER_OPTIC, GAS, WATER}
 
 public class UtilityLine implements Mappable{
@@ -14,16 +16,28 @@ public class UtilityLine implements Mappable{
 
     @Override
     public String getLabel() {
-        return "";
+        return name + " (" + type +")";
     }
 
     @Override
     public Geometry getShape() {
-        return null;
+        return Geometry.LINE;
     }
 
     @Override
     public String getMarker() {
-        return "";
+        return switch (type) {
+            case ELECTRICAL -> Color.RED + " "  + LineMarker.DASHED;
+            case FIBER_OPTIC -> Color.GREEN + " " + LineMarker.DOTTED;
+            case GAS -> Color.ORANGE + " " + LineMarker.SOLID;
+            case WATER -> Color.BLUE + " " + LineMarker.SOLID;
+            default -> Color.BLACK + " " + LineMarker.SOLID;
+        };
+    }
+
+    @Override
+    public String toJSON() {
+        return Mappable.super.toJSON() + """
+                , "name": "%s", "utility": "%s" """.formatted(name, type);
     }
 }
