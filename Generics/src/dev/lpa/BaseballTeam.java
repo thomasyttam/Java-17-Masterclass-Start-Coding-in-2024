a563bf64d875e7f6dd99f6f7c5519d3dc381d6db
Thomas Tam <tyttam@gmail.com>
2025-01-22 06:47:04 -0500

    Generics

---
+++ Generics/src/dev/lpa/BaseballTeam.java
@@ -0,0 +1,23 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballTeam {
+
+    private String teamName;
+    private List<BaseballPlayer> teamMembers = new ArrayList<>();
+    private int totalWins = 0;
+    private int totalLosses = 0;
+    private int totalTies = 0;
+
+    public BaseballTeam(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public void addTeamMember(BaseballPlayer player) {
+        if(teamMembers.contains(player)) {
+            teamMembers.add(player);
+        }
+    }
+}
---
+++ Generics/src/dev/lpa/Main.java
@@ -0,0 +1,10 @@
+package dev.lpa;
+
+record BaseballPlayer(String name, String position){}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
