6fdeb559097c3af370843cd85fe3f15108232d5e
Thomas Tam <tyttam@gmail.com>
2025-01-24 06:41:44 -0500

    Generics

--- Generics/src/dev/lpa/BaseballTeam.java
+++ Generics/src/dev/lpa/BaseballTeam.java
@@ -16,17 +16,39 @@ public class BaseballTeam {
     }
 
     public void addTeamMember(BaseballPlayer player) {
-        if(teamMembers.contains(player)) {
+
+        if(!teamMembers.contains(player)) {
             teamMembers.add(player);
         }
     }
 
     public void printTeamMembers() {
+
         System.out.println(teamName + " Roaster:");
         System.out.println(teamMembers);
     }
 
     public int ranking() {
         return (totalLosses * 2) + totalTies + 1;
+    }
+
+    public String setScore(int ourScore, int theirScore) {
+
+        String message = "lost to";
+        if(ourScore > theirScore) {
+            totalWins++;
+            message = "beat";
+        } else if (ourScore == theirScore) {
+            totalTies++;
+            message = "tied";
+        } else {
+            totalLosses++;
+        }
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return teamName + " (Ranked " + ranking() + ")";
     }
 }
