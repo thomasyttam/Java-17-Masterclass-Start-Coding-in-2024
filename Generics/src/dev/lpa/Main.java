9632f62f9ae7c8bc77a0e5863baf3e2d978b6433
Thomas Tam <tyttam@gmail.com>
2025-01-25 16:42:08 -0500

    Generics

--- Generics/src/dev/lpa/Main.java
+++ Generics/src/dev/lpa/Main.java
@@ -1,13 +1,18 @@
 package dev.lpa;
 
-record BaseballPlayer(String name, String position){}
+interface Player {}
+record BaseballPlayer(String name, String position) implements Player{}
 
 public class Main {
 
     public static void main(String[] args) {
 
-        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
-        BaseballTeam astros = new BaseballTeam("Huston Astros");
+        BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros1 = new BaseballTeam("Huston Astros");
+        scoreResult(phillies1, 3, astros1, 5);
+
+        SportsTeam phillies = new SportsTeam("Philadelphia Phillies");
+        SportsTeam astros = new SportsTeam("Huston Astros");
         scoreResult(phillies, 3, astros, 5);
 
         var harper = new BaseballPlayer("B Harper", "Right Fielder");
@@ -15,6 +20,13 @@ public class Main {
         phillies.addTeamMember(harper);
         phillies.addTeamMember(marsh);
         phillies.printTeamMembers();
+    }
+
+    public static void scoreResult(SportsTeam team1, int t1_score,
+                                   SportsTeam team2, int t2_score) {
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
     }
 
     public static void scoreResult(BaseballTeam team1, int t1_score,
