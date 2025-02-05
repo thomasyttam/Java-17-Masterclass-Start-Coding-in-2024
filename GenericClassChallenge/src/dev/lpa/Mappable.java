b23badf9502b4f351bb38793e3fac46d3ef73ea2
Thomas Tam <tyttam@gmail.com>
2025-02-05 07:52:22 -0500

    Generic Class Challenge

--- GenericClassChallenge/src/dev/lpa/Mappable.java
+++ GenericClassChallenge/src/dev/lpa/Mappable.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public interface Mappable {
 
     void render();
@@ -16,4 +18,41 @@ public interface Mappable {
 abstract class Point implements Mappable {
 
     private double[] location = new double[2];
+
+    public Point(String location){
+        this.location = Mappable.stringToLatLon(location);
+    }
+
+    @Override
+    public void render() {
+
+        System.out.println("Render" + this + " as POINT (" + location() + ")");
+    }
+
+    private String location() {
+        return Arrays.toString(location);
+    }
+}
+
+
+abstract class Line implements Mappable {
+
+    private double[][] locations;
+
+    public Line(String... locations) {
+        this.locations = new double[locations.length][];
+        int index = 0;
+        for(var l : locations) {
+            this.locations[index++] = Mappable.stringToLatLon(l);
+        }
+    }
+
+    @Override
+    public void render() {
+        System.out.println("Render" + this + " as LINE (" + locations() + ")");
+    }
+
+    private String locations() {
+        return Arrays.deepToString(locations);
+    }
 }
