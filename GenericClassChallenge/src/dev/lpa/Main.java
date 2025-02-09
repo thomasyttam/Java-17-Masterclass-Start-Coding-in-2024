da8479f0d3e68c259c0fee15f3573485d382b670
Thomas Tam <tyttam@gmail.com>
2025-02-09 11:03:17 -0500

    Generic Class Challenge

--- GenericClassChallenge/src/dev/lpa/Main.java
+++ GenericClassChallenge/src/dev/lpa/Main.java
@@ -4,10 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var NationalParks = new Park[]{
+        var nationalUSParks = new Park[]{
                 new Park("Yellowstone", "44.4882, -110.5916"),
                 new Park("Grand Canyon", "36.1085, -112.0965"),
                 new Park("Yosemite", "37.8855, -119.5360")
+        };
+
+        Layer<Park> parkLayer = new Layer<>(nationalUSParks);
+        parkLayer.renderLayer();
+
+        var majorUSRivers = new River[]{
+
         };
     }
 }
