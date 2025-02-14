1ab310763c51311340b61c9f460836898cb3bbdf
Thomas Tam <tyttam@gmail.com>
2025-02-14 07:20:56 -0500

    Comparing

--- Comparing/src/dev/lpa/Main.java
+++ Comparing/src/dev/lpa/Main.java
@@ -3,6 +3,17 @@ package dev.lpa;
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Integer five = 5;
+        Integer[] others = {0, 5, 10, -50, 50};
+
+        for(Integer i : others) {
+            int val = five.compareTo(i);
+            System.out.printf("%d %s %d: compareTo=%d%n", five,
+                    (val == 0 ? "==" : (val < 0) ? "<" : ">"), i, val);
+        }
+
+        String banana = "banana";
+        String[] fruit = {"apple", "banana", "pear", "BANANA"};
     }
 }
