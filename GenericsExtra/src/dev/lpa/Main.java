ae31c4d789e72c08bc4360cf9f21b389e5e86c6f
Thomas Tam <tyttam@gmail.com>
2025-02-23 17:19:50 -0500

    GenericsExtra

--- GenericsExtra/src/dev/lpa/Main.java
+++ GenericsExtra/src/dev/lpa/Main.java
@@ -1,8 +1,28 @@
 package dev.lpa;
 
+import dev.lpa.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int studentCount = 10;
+        List<Student> students = new ArrayList<>();
+        for (int i = 0; i < studentCount; i++) {
+            students.add(new Student());
+        }
+
+        printList(students);
+    }
+
+    public static void printList(List<Student> students) {
+
+        for (var student : students) {
+            System.out.println(student);
+        }
+        System.out.println();
     }
 }
