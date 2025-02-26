c328b525906db888e644d3e53b648e133589ccc0
Thomas Tam <tyttam@gmail.com>
2025-02-26 07:24:21 -0500

    GenericsExtra

--- GenericsExtra/src/dev/lpa/Main.java
+++ GenericsExtra/src/dev/lpa/Main.java
@@ -17,21 +17,65 @@ public class Main {
             students.add(new Student());
         }
         students.add(new LPAStudent());
-        printList(students);
+//        printList(students);
+        printMoreList(students);
 
         List<LPAStudent> lpaStudents = new ArrayList<>();
         for (int i = 0; i < studentCount; i++) {
             lpaStudents.add(new LPAStudent());
         }
-        printList(lpaStudents);
+//        printList(lpaStudents);
+        printMoreList(lpaStudents);
+
+        testList(new ArrayList<String >(List.of("Able", "Barry", "Charlie")));
+        testList(new ArrayList<Integer>(List.of(1, 2, 3)));
+
     }
 
-//    public static void printList(List<Student> students) {
-    public static void printList(List students) {
+    public static void printMoreList(List<? extends Student> students) {
+
+//        Student last = students.get(students.size() - 1);
+//        students.set(0, last);
 
         for (var student : students) {
+//            System.out.println(student.getYearStarted() + ": " + student);
             System.out.println(student);
         }
         System.out.println();
     }
+
+    public static void testList(List<?> list) {
+
+        for(var element : list) {
+            if(element instanceof String s) {
+                System.out.println("String: " + s.toUpperCase());
+            } else if (element instanceof Integer i) {
+                System.out.println("Integer: " + i.floatValue());
+            }
+        }
+    }
+
+//    public static void testList(List<String> list) {
+//
+//        for(var element :  list) {
+//            System.out.println("String: " + element.toUpperCase());
+//        }
+//    }
+//
+//    public static void testList(List<Integer> list) {
+//
+//        for(var element :  list) {
+//            System.out.println("String: " + element.floatValue());
+//        }
+//    }
+
+//    public static void printList(List<Student> students) {
+//  public static void printList(List students) {
+//    public static <T extends Student> void printList(List<T> students) {
+//
+//        for (var student : students) {
+//            System.out.println(student.getYearStarted() + ": " + student);
+//        }
+//        System.out.println();
+//    }
 }
