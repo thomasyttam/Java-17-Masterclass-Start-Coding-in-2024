8465a666cb6c1f6e9a8566195fa3dac57c342d6e
Thomas Tam <tyttam@gmail.com>
2025-03-06 07:16:43 -0500

    NestedClasses

--- NestedClasses/src/dev/lpa/Main.java
+++ NestedClasses/src/dev/lpa/Main.java
@@ -1,8 +1,27 @@
 package dev.lpa;
 
+import dev.lpa.domain.Employee;
+import dev.lpa.domain.EmployeeComparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        List<Employee> employees = new ArrayList<>(List.of(
+                new Employee(10001, "Ralph", 2015),
+                new Employee(10005, "Carole", 2021),
+                new Employee(10022, "Jane", 2013),
+                new Employee(13151, "Laura", 2020),
+                new Employee(10050, "Jim", 2018)));
+
+        var comparator = new EmployeeComparator<>();
+        employees.sort(comparator);
+
+        for (Employee e : employees) {
+            System.out.println(e);
+        }
     }
 }
