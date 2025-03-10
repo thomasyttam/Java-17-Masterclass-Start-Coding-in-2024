5c50785b04b9ba07561ad3ecbe490c1bfe6b4bd3
Thomas Tam <tyttam@gmail.com>
2025-03-10 06:41:22 -0400

    NestedClasses

--- NestedClasses/src/dev/lpa/domain/StoreEmployee.java
+++ NestedClasses/src/dev/lpa/domain/StoreEmployee.java
@@ -1,6 +1,18 @@
 package dev.lpa.domain;
 
+import java.util.Comparator;
+
 public class StoreEmployee extends Employee{
 
     private String store;
+
+    public class StoreComparator<T extends StoreComparator>
+    implements Comparator<StoreEmployee> {
+
+        @Override
+        public int compare(StoreEmployee o1, StoreEmployee o2) {
+            int result = o1.store.compareTo(o2.store);
+            return result;
+        }
+    }
 }
