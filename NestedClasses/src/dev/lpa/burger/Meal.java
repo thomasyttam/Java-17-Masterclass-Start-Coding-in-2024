aa0c82c96c5fe0a842e79e456ac154cac20740f8
Thomas Tam <tyttam@gmail.com>
2025-03-13 07:23:40 -0400

    NestedClasses

--- NestedClasses/src/dev/lpa/burger/Meal.java
+++ NestedClasses/src/dev/lpa/burger/Meal.java
@@ -2,6 +2,23 @@ package dev.lpa.burger;
 
 public class Meal {
 
+    private double base = 5.0;
+    private Item burger;
+    private Item drink;
+    private Item side;
+
+    public Meal() {
+        burger = new Item("regular", "burger");
+        drink = new Item("coke", "drink", 1.5);
+        System.out.println(drink.name);
+        side = new Item("fries", "side", 2.0);
+    }
+
+    @Override
+    public String toString() {
+        return "%s%n%s%n%s%n".formatted(burger, drink, side);
+    }
+
     private class Item{
 
         private String name;
@@ -9,13 +26,20 @@ public class Meal {
         private double price;
 
         public Item(String name, String type) {
-            this(name, type, 0);
+            // can access private attribute in same nested class -> below is base
+            this(name, type, type.equals("burger") ? base : 0);
+//            this(name, type, 0);
         }
-        
+
         public Item(String name, String type, double price) {
             this.name = name;
             this.type = type;
             this.price = price;
+        }
+
+        @Override
+        public String toString() {
+            return "%10s%15s $%.2f".formatted(type, name, price);
         }
     }
 }
---
+++ NestedClasses/src/dev/lpa/burger/Store.java
@@ -0,0 +1,10 @@
+package dev.lpa.burger;
+
+public class Store {
+
+    public static void main(String[] args) {
+
+        Meal regularMeal = new Meal();
+        System.out.println(regularMeal);
+    }
+}
