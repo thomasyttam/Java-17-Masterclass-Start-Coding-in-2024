a7f02f3f676a59f8876cb03f670ca243faaeb6c2
Thomas Tam <tyttam@gmail.com>
2025-03-15 17:10:30 -0400

    NestedClasses

--- NestedClasses/src/dev/lpa/burger/Meal.java
+++ NestedClasses/src/dev/lpa/burger/Meal.java
@@ -1,9 +1,13 @@
 package dev.lpa.burger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
 
     private double price = 5.0;
-    private Item burger;
+//    private Item burger;
+    private Burger burger;
     private Item drink;
     private Item side;
 
@@ -15,14 +19,15 @@ public class Meal {
 
     public Meal(double conversionRate) {
         this.conversionRate = conversionRate;
-        burger = new Item("regular", "burger");
+//        burger = new Item("regular", "burger");
+        burger = new Burger("regular");
         drink = new Item("coke", "drink", 1.5);
         System.out.println(drink.name);
         side = new Item("fries", "side", 2.0);
     }
 
     public double getTotal() {
-        double total = burger.price + drink.price + side.price;
+        double total = burger.getPrice() + drink.price + side.price;
         return Item.getPrice(total, conversionRate);
     }
 
@@ -59,5 +64,20 @@ public class Meal {
         private static double getPrice(double price, double rate) {
             return price * rate;
         }
+    }
+
+    private class Burger extends Item {
+
+        private enum Extra {AVOCADO, BACON, CHEESE, KETCHUP, MAYO, MUSTARD, PICKLES}
+
+        private List<Item> toppings = new ArrayList<>();
+        Burger(String name) {
+            super(name, "burger", 5.0   );
+        }
+
+        public double getPrice() {
+            return super.price;
+        }
+
     }
 }
