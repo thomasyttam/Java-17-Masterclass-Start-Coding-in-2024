bea8a2a8525cec309daf56850fc3e10d11a6ccbf
Thomas Tam <tyttam@gmail.com>
2025-03-24 07:22:46 -0400

    LocalClassChallenge

---
+++ LocalClassChallenge/src/dev/lpa/Main.java
@@ -0,0 +1,16 @@
+package dev.lpa;
+
+import dev.lpa.domain.Employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Minnie", "Mouse", "01/02/2015");
+        Employee e2 = new Employee("Mickie", "Mouse", "05/08/2000");
+        Employee e3 = new Employee("Daffy", "Duck", "11/02/2011");
+        Employee e4 = new Employee("Daisy", "Duck", "05/03/2012");
+        Employee e5 = new Employee("Goofy", "Dog", "23/07/2020");
+
+    }
+}
