88f5d7ecdcf185122dc18ef00245bcece30b8b01
Thomas Tam <tyttam@gmail.com>
2025-03-26 07:26:13 -0400

    LocalClassChallenge

--- LocalClassChallenge/src/dev/lpa/Main.java
+++ LocalClassChallenge/src/dev/lpa/Main.java
@@ -5,6 +5,7 @@ import dev.lpa.domain.Employee;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -18,6 +19,10 @@ public class Main {
         Employee e5 = new Employee("Goofy", "Dog", "23/07/2020");
 
         List<Employee> list = new ArrayList<>(Arrays.asList(e1, e2, e3, e4, e5));
+
+        printOrderedList(list, "name");
+        System.out.println();
+        printOrderedList(list, "year");
     }
 
     public static void printOrderedList(List<Employee> elist, String sortField) {
@@ -32,7 +37,40 @@ public class Main {
 
             public MyEmployee(Employee containedEmployee) {
                 this.containedEmployee = containedEmployee;
+                yearsWorked = currentYear - Integer.parseInt(
+                        containedEmployee.hireDate().split("/")[2]);
+                fullName = String.join(" ",
+                         containedEmployee.first(), containedEmployee.last());
             }
+
+            @Override
+            public String toString() {
+                return "%s has been an employee for %d years".formatted(
+                        fullName, yearsWorked);
+            }
+        }
+
+        List<MyEmployee> list = new ArrayList<>();
+        for(Employee employee : elist) {
+            list.add(new MyEmployee(employee));
+        }
+
+        var comparator = new Comparator<MyEmployee>() {
+
+            @Override
+            public int compare(MyEmployee o1, MyEmployee o2) {
+
+                if (sortField.equals("name")) {
+                    return o1.fullName.compareTo(o2.fullName);
+                }
+                return o1.yearsWorked - o2.yearsWorked;
+            }
+        };
+
+        list.sort(comparator);
+
+        for(MyEmployee myEmployee : list) {
+            System.out.println(myEmployee);
         }
     }
 }
