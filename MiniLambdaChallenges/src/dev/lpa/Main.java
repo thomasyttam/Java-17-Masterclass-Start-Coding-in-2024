b99186365a57055bf77c8b22de38be80dd07e153
Thomas Tam <tyttam@gmail.com>
2025-04-02 06:45:02 -0400

    MiniLambdaChallenges

---
+++ MiniLambdaChallenges/src/dev/lpa/Main.java
@@ -0,0 +1,48 @@
+package dev.lpa;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Consumer<String> printWords = new Consumer<String>() {
+            @Override
+            public void accept(String sentence) {
+                String[] parts = sentence.split(" ");
+                for (String part : parts) {
+                    System.out.println(part);
+                }
+            }
+        };
+
+        Consumer<String> printWordsLambda = sentence -> {
+            String[] parts = sentence.split(" ");
+            for (String part : parts) {
+                System.out.println(part);
+            }
+        };
+
+        printWords.accept("Let's split this up into an array");
+        printWordsLambda.accept("Let's split this up into an array");
+
+        Consumer<String> printWordsForEach = sentence -> {
+            String[] parts = sentence.split(" ");
+            Arrays.asList(parts).forEach(s -> System.out.println(s));
+        };
+
+        printWordsForEach.accept("Let's split this up into an array");
+    }
+
+    public static String EverySecondChar(String source) {
+
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    }
+}
