e1ad45d3be20b0311fc9e28353707732d9ac4c4d
Thomas Tam <tyttam@gmail.com>
2025-04-06 08:38:21 -0400

    LambdaExpressionChallenge

--- LambdaExpressionChallenge/src/dev/lpa/Main.java
+++ LambdaExpressionChallenge/src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
@@ -8,5 +10,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String[] names = {"Anna", "Bob", "Corale", "David", "Ed", "Fred", "Gary"};
+
+        System.out.println("--> Original strings");
+        System.out.println(Arrays.toString(names));
+
+        Arrays.setAll(names, i -> names[i].toUpperCase());
+        System.out.println("--> Transform to Uppercase");
+        System.out.println(Arrays.toString(names));
+
+        List<String> backedByArray = Arrays.asList(names);
+
+    }
+
+    // get the random char between startChar and endChar
+    public static char getRandomChar(char startChar, char endChar) {
+        return (char) random.nextInt((int) startChar, (int) endChar + 1);
+    }
+
+    // reverse the first name and return the reversed string
+    public static String getReversedName(String firstname) {
+        return new StringBuilder(firstname).reverse().toString();
     }
 }
