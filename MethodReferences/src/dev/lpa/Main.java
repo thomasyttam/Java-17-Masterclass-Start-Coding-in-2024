d23d6ce068225d6a2ee6f2be9203f89542800ad0
Thomas Tam <tyttam@gmail.com>
2025-04-09 07:22:29 -0400

    MethodReferences

--- MethodReferences/src/dev/lpa/Main.java
+++ MethodReferences/src/dev/lpa/Main.java
@@ -3,8 +3,10 @@ package dev.lpa;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 class PlainOld {
 
@@ -38,6 +40,25 @@ public class Main {
 
         System.out.println("Getting array");
         PlainOld[] pojo1 = seedArray(PlainOld::new, 10);
+
+        calculator((s1, s2) -> s1 + s2, "Hello ", "World");
+        calculator((s1, s2) -> s1.concat(s2), "Hello ", "World");
+        calculator(String::concat, "Hello ", "World");
+
+        BinaryOperator<String> b1 = (s1, s2) -> s1.concat(s2);
+        BinaryOperator<String> b11 = String::concat;
+        BiFunction<String, String, String> b2 = (s1, s2) -> s1.concat(s2);
+        BiFunction<String, String, String> b22 = String::concat;
+
+        UnaryOperator<String> u1 = s1 -> s1.toUpperCase();
+        UnaryOperator<String> u11 = String::toUpperCase;
+
+        System.out.println(b1.apply("Hello ", "World"));
+        System.out.println(b2.apply("Hello ", "World"));
+        System.out.println(u1.apply("Hello "));
+
+        String result = "Hello".transform(u1);
+        System.out.println("Result = " + result);
     }
 
     private static <T> void calculator(BinaryOperator<T> function, T value1, T value2) {
