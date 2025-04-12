49ce04a166c61accb8c9d1a2769789217f9a3e9b
Thomas Tam <tyttam@gmail.com>
2025-04-12 10:03:12 -0400

    MethodReferenceChallenge

--- MethodReferenceChallenge/src/dev/lpa/Main.java
+++ MethodReferenceChallenge/src/dev/lpa/Main.java
@@ -10,6 +10,13 @@ public class Main {
 
     private static Random newRandom = new Random();
 
+    private record Person(String first) {
+
+        public String last(String s) {
+            return first + " " + s.substring(0, s.indexOf(" "));
+        }
+    }
+
     public static void main(String[] args) {
 
 //        List<String> nameslist = new ArrayList<>(List.of("Anna", "Bob", "Charlie", "David",
@@ -21,9 +28,17 @@ public class Main {
 
         String[] names = {"Anna", "Bob", "Cameron", "Donald", "Eva", "Francis"};
 
+        Person tim = new Person("Tim");
         List<UnaryOperator<String>> list = new ArrayList<>(List.of(
                 String::toUpperCase,
-                s -> s += " " + getRandomChar('A', 'Z') + "."
+                s -> s += " " + getRandomChar('A', 'Z') + ".",
+                s -> s += " " + reverse(s, 0, s.indexOf(" ")),
+                Main::reverse,
+                String::new,
+//                s -> new String("Howdy" + s),
+                String::valueOf,
+                tim::last,
+                (new Person("MARY"))::last
         ));
 
         applyChanges(names, list);
@@ -41,6 +56,10 @@ public class Main {
 
      private static char getRandomChar(char startChar, char endChar) {
         return (char) newRandom.nextInt((int) startChar, (int) endChar +1);
+     }
+
+     private static String reverse(String s) {
+        return reverse(s, 0 , s.length());
      }
 
      private static String reverse(String s, int start, int end) {
