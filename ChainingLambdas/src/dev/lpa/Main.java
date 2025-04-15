3d796a7d34899caba32b164c8b2922bc6aa457dd
Thomas Tam <tyttam@gmail.com>
2025-04-15 06:36:58 -0400

    ChainingLambdas

--- ChainingLambdas/src/dev/lpa/Main.java
+++ ChainingLambdas/src/dev/lpa/Main.java
@@ -1,7 +1,9 @@
 package dev.lpa;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -28,5 +30,28 @@ public class Main {
                 .andThen(s -> s. split(" "))
                 .andThen(s -> s[1].toUpperCase() + ", " + s[0]);
         System.out.println(f1.apply(name));
+
+        Function<String, Integer> f2 = uCase
+                .andThen(s -> s.concat(" Buchalka"))
+                .andThen(s -> s. split(" "))
+                .andThen(s -> String.join(", ", s))
+                .andThen(String::length);
+        System.out.println(f2.apply(name));
+
+        String[] names = {"Ann", "Bob", "Carol"};
+        Consumer<String> s0 = s -> System.out.print(s.charAt(0));
+        Consumer<String> s1 = System.out::println;
+        Arrays.asList(names).forEach(s0
+                .andThen(s -> System.out.print(" - "))
+                .andThen(s1));
+
+        Predicate<String> p1 = s -> s.equals("TIM");
+        Predicate<String> p2 = s -> s.equalsIgnoreCase("TIM");
+        Predicate<String> p3 = s -> s.startsWith("T");
+        Predicate<String> p4 = s -> s.endsWith("e");
+
+        Predicate<String> combined1 = p1.or(p2);
+        System.out.println("combined1 = " + combined1.test(name));
+
     }
 }
