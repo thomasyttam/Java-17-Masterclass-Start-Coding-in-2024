208d55e3a617d9d3723d9be5a12ba27d62a84e16
Thomas Tam <tyttam@gmail.com>
2025-04-16 06:01:15 -0400

    CollectionsOverview

--- CollectionsOverview/src/dev/lpa/Main.java
+++ CollectionsOverview/src/dev/lpa/Main.java
@@ -1,12 +1,31 @@
 package dev.lpa;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+//        List<String> list = new ArrayList<>();
+//        Collection<String> list = new ArrayList<>();
+//        Collection<String> list = new TreeSet<>();
+        Collection<String> list = new HashSet<>();
+
+        String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
+        list.addAll(Arrays.asList(names));
+        System.out.println(list);
+
+        list.add("Fred");
+        list.addAll(Arrays.asList("George", "Gary", "Grace"));
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+
+        list.removeIf(s -> s.charAt(0) == 'G');
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+//        list.sort(); -> no sort method in Collection
+
     }
 }
