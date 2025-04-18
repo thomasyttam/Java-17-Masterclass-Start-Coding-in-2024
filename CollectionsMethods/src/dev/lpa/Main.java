b8a49a3d0a0be9d0387d0311ebae48ef736f4afb
Thomas Tam <tyttam@gmail.com>
2025-04-18 10:39:51 -0400

    CollectionsMethods

--- CollectionsMethods/src/dev/lpa/Main.java
+++ CollectionsMethods/src/dev/lpa/Main.java
@@ -1,6 +1,8 @@
 package dev.lpa;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -16,5 +18,19 @@ public class Main {
         Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
         Arrays.fill(cardArray, aceOfHearts);
         Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+
+        List<Card> cards = new ArrayList<>(52); // 52 will not set 52 null elements, size still zero
+        Collections.fill(cards, aceOfHearts); // replace the element, but size still zero
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+
+        Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
+
+        Collections.addAll(cards, cardArray);
     }
 }
