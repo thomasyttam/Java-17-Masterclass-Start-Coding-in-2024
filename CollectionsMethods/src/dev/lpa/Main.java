f76f1a6b8ac7494a538dc26a267b6b4105786b7a
Thomas Tam <tyttam@gmail.com>
2025-04-20 10:46:41 -0400

    CollectionsMethods

--- CollectionsMethods/src/dev/lpa/Main.java
+++ CollectionsMethods/src/dev/lpa/Main.java
@@ -1,18 +1,12 @@
 package dev.lpa;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         //        Card.printDeck(Card.getStandardDeck());
-
-        List<Card> deck = Card.getStandardDeck();
-        Card.printDeck(deck);
 
         Card[] cardArray = new Card[13];
         Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
@@ -42,5 +36,45 @@ public class Main {
         cards = List.copyOf(kingsOfClubs); //this method returns an unmodifiable list
         Card.printDeck(cards, "List Copy of Kings", 2);
         System.out.println("cards.size() = " + cards.size());
+
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+
+        Collections.shuffle(deck);
+        Card.printDeck(deck, "Shuffled Deck", 4);
+
+        Collections.reverse(deck);
+        Card.printDeck(deck, "Reversed Deck of Cards", 4);
+
+        var sortingAlgorithm = Comparator.comparing(Card::rank)
+                .thenComparing(Card::suit);
+        Collections.sort(deck, sortingAlgorithm);
+        Card.printDeck(deck, "Standard Deck sorted by rank, suit", 13);
+
+        Collections.reverse(deck);
+        Card.printDeck(deck, "Sorted by rank, suit reversed", 13);
+
+        List<Card> kings = new ArrayList<>(deck.subList(4, 8));
+        Card.printDeck(kings, "Kings in deck", 1);
+
+        List<Card> tens = new ArrayList<>(deck.subList(16, 20));
+        Card.printDeck(tens, "Tens in deck", 1);
+
+        int subListIndex = Collections.indexOfSubList(deck, tens);
+        System.out.println("sublist index for tens = " + subListIndex);
+        System.out.println("Contains = " + deck.containsAll(tens));
+
+        Collections.shuffle(deck);
+        int subListIndex2 = Collections.indexOfSubList(deck, tens);
+        System.out.println("sublist index for tens = " + subListIndex2); // cannot find since shuffle
+        System.out.println("Contains = " + deck.containsAll(tens)); // true, still find all elements in tens
+
+        boolean disjoint = Collections.disjoint(deck, tens);
+        System.out.println("disjoint = " + disjoint);
+
+        boolean disjoint2 = Collections.disjoint(kings, tens);
+        System.out.println("disjoint = " + disjoint2);
+
+
     }
 }
