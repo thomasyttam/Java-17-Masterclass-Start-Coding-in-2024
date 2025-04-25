f3d2bc053d2873040bf5c77dbe820afd4a7d43a0
Thomas Tam <tyttam@gmail.com>
2025-04-25 05:05:51 -0400

    CollectionsMethods

--- CollectionsMethods/src/dev/lpa/games/GameController.java
+++ CollectionsMethods/src/dev/lpa/games/GameController.java
@@ -1,8 +1,12 @@
 package dev.lpa.games;
 
+import dev.lpa.games.poker.PokerGame;
+
 public class GameController {
 
     public static void main(String[] args) {
 
+        PokerGame fiveCardDraw = new PokerGame(4, 5);
+        fiveCardDraw.startPlay();
     }
 }
--- CollectionsMethods/src/dev/lpa/games/poker/PokerGame.java
+++ CollectionsMethods/src/dev/lpa/games/poker/PokerGame.java
@@ -2,7 +2,7 @@ package dev.lpa.games.poker;
 
 import dev.lpa.Card;
 
-import java.util.List;
+import java.util.*;
 
 public class PokerGame {
 
@@ -11,4 +11,37 @@ public class PokerGame {
     private int cardsInHand;
     private List<PokerHand> pokerHands;
     private List<Card> remainingCards;
+
+    public PokerGame(int playerCount, int cardsInHand) {
+        this.playerCount = playerCount;
+        this.cardsInHand = cardsInHand;
+        pokerHands = new ArrayList<>(cardsInHand);
+    }
+
+    public void startPlay() {
+
+        Collections.shuffle(deck);
+        Card.printDeck(deck);
+        int randomMiddle = new Random().nextInt(15, 35);
+        Collections.rotate(deck, randomMiddle);
+        Card.printDeck(deck);
+
+        deal();
+        System.out.println("-".repeat(30));
+        pokerHands.forEach(System.out::println);
+    }
+
+    private void deal() {
+        Card[][] hands = new Card[playerCount][cardsInHand];
+        for (int deckIndex = 0, i = 0; i < cardsInHand; i++) {
+            for (int j = 0; j < playerCount; j++) {
+                hands[j][i] = deck.get(deckIndex++);
+            }
+        }
+
+        int playerNo = 1;
+        for (Card[] hand : hands) {
+            pokerHands.add(new PokerHand(playerNo++, Arrays.asList(hand)));
+        }
+    }
 }
