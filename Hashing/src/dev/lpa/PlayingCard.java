1e9ce52005e2580e0ea77f0b14a2434d80790d80
Thomas Tam <tyttam@gmail.com>
2025-05-05 06:07:04 -0400

    Hashing

--- Hashing/src/dev/lpa/PlayingCard.java
+++ Hashing/src/dev/lpa/PlayingCard.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Objects;
+
 public class PlayingCard {
 
     private String suit;
@@ -18,16 +20,33 @@ public class PlayingCard {
         return face + " of " + suit;
     }
 
+//    @Override
+////    public int hashCode() {
+//////        return super.hashCode();
+////        return internalHash;
+////    }
+////
+////    @Override
+////    public boolean equals(Object obj) {
+//////        return super.equals(obj);
+////        System.out.println("---> Checking Playing Card equality");
+////        return true;
+////    }
+
+
     @Override
-    public int hashCode() {
-//        return super.hashCode();
-        return internalHash;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlayingCard that = (PlayingCard) o;
+        return Objects.equals(suit, that.suit) && Objects.equals(face, that.face);
     }
 
     @Override
-    public boolean equals(Object obj) {
-//        return super.equals(obj);
-        System.out.println("---> Checking Playing Card equality");
-        return true;
+    public int hashCode() {
+        int result = Objects.hashCode(suit);
+        result = 31 * result + Objects.hashCode(face);
+        return result;
     }
 }
