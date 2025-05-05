153918f7779e3707257599276756e1c7b62ef542
Thomas Tam <tyttam@gmail.com>
2025-05-05 06:27:51 -0400

    SetsAndMaps

---
+++ SetsAndMaps/src/dev/lpa/Contact.java
@@ -0,0 +1,11 @@
+package dev.lpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Contact {
+
+    private String name;
+    private Set<String> emails = new HashSet<>();
+    private Set<String> phones = new HashSet<>();
+}
