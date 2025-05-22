1c90ccf73f6d79c2c0a3202ee47c46c6ad62b525
Thomas Tam <tyttam@gmail.com>
2025-05-22 07:03:46 -0400

    LinkedHashSet and TreeSet

---
+++ SetsAndMaps/src/dev/lpa/TreeSetMain.java
@@ -0,0 +1,25 @@
+package dev.lpa;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class TreeSetMain {
+
+    public static void main(String[] args) {
+
+        List<Contact> phones = ContactData.getData("phone");
+        List<Contact> emails = ContactData.getData("email");
+
+//        NavigableSet<Contact> sorted = new TreeSet<>(phones);
+        Comparator<Contact> mySort = Comparator.comparing(Contact::getName);
+        NavigableSet<Contact> sorted = new TreeSet<>(mySort);
+        sorted.addAll(phones);
+        sorted.forEach(System.out::println);
+
+        NavigableSet<String> justNames = new TreeSet<>();
+        phones.forEach(c -> justNames.add(c.getName()));
+        System.out.println(justNames);
+    }
+}
