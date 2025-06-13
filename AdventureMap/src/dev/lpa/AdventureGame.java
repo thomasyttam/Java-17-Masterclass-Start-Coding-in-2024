88554ac360b841d57ec981f64682d7a34d207a5c
Thomas Tam <tyttam@gmail.com>
2025-06-13 06:14:24 -0400

    AdventureMap

--- AdventureMap/src/dev/lpa/AdventureGame.java
+++ AdventureMap/src/dev/lpa/AdventureGame.java
@@ -1,6 +1,8 @@
 package dev.lpa;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AdventureGame {
@@ -31,16 +33,40 @@ public class AdventureGame {
     private Map<String, Location> adventureMap = new HashMap<>();
 
     public AdventureGame() {
+        this(null);
     }
 
     public AdventureGame(String customLocations) {
-
+        loadLocations(GAME_LOCATIONS);
+        if (customLocations != null) {
+            loadLocations(customLocations);
+        }
     }
 
     public void loadLocations(String data) {
 
         for (String s :  data.split("\\R")) {
-
+            String[] parts = s.split(",", 3);
+            Arrays.asList(parts).replaceAll(String::trim);
+            Map<Compass, String> nextPlaces = loadDirections(parts[2]);
+            Location location = new Location(parts[1], nextPlaces);
+            adventureMap.put(parts[0], location);
         }
+        adventureMap.forEach((k, v) -> System.out.printf("%s:%s%n", k, v));
+    }
+
+    private Map<Compass, String>  loadDirections(String nextPlaces) {
+
+        Map<Compass, String> directions = new HashMap<>();
+        List<String> nextSteps = Arrays.asList(nextPlaces.split(","));
+
+        nextSteps.replaceAll(String::trim);
+        for (String nextPlace : nextSteps) {
+            String[] splits = nextPlace.split(":");
+            Compass compass = Compass.valueOf(splits[0].trim());
+            String destination = splits[1].trim();
+            directions.put(compass, destination);
+        }
+        return directions;
     }
 }
