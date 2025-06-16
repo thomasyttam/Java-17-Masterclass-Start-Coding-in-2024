ddf72c7c5bf0489c360299af358e03da6be54b01
Thomas Tam <tyttam@gmail.com>
2025-06-16 06:24:37 -0400

    AdventureMap

--- AdventureMap/src/dev/lpa/Main.java
+++ AdventureMap/src/dev/lpa/Main.java
@@ -6,13 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        AdventureGame game = new AdventureGame();
-        game.play("road");
+        String myLocations = """        
+                lake,at the edge of Lake Tim,E:ocean,W:forest,S:well house,N:cave
+                ocean,on Tim's beach before an angry sea,W:lake
+                cave,at the mouth of Tim's bat cave,E:ocean,W:forest,S:lake
+                """;
+
+        AdventureGame game = new AdventureGame(myLocations); // add extra locations
+        game.play("lake");
+
+//        AdventureGame game = new AdventureGame();
+//        game.play("road");
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            String direction = scanner.nextLine().trim().toUpperCase().substring(0, 1); // cannot handle "\n"
+            String direction = scanner.nextLine().trim().toUpperCase().substring(0, 1); // need to do input validation "\n"
             if (direction.equals("Q")) break;
             game.move(direction);
         }
