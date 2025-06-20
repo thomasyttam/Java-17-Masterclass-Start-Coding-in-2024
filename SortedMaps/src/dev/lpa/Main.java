7850cbe085e0a4762e916d2702586ff630ce819e
Thomas Tam <tyttam@gmail.com>
2025-06-20 04:54:09 -0400

    SortedMaps

--- SortedMaps/src/dev/lpa/Main.java
+++ SortedMaps/src/dev/lpa/Main.java
@@ -21,9 +21,44 @@ public class Main {
         addPurchase("Joe Jones", jmc, 149.99);
         addPurchase("Bill Brown", python, 119.99);
 
+        addPurchase("Chuck Cheese", python, 119.99);
+        addPurchase("Davey Jones", jmc, 139.99);
+        addPurchase("Eva East", python, 139.99);
+        addPurchase("Fred Forker", jmc, 139.99);
+        addPurchase("Greg Brady", python, 129.99);
+
         purchases.forEach((key, value) -> System.out.println(key + ": " + value));
         System.out.println("-----------------------");
         students.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        NavigableMap<LocalDate,List<Purchase>> datedPurchases = new TreeMap<>();
+
+        for (Purchase p : purchases.values() ) {
+            datedPurchases.compute(p.purchaseDate(),
+                    (pdate, plist) -> {
+                        List<Purchase> list =
+                                (plist == null) ? new ArrayList<>() : plist;
+                        list.add(p);
+                        return list;
+                    });
+        }
+
+        datedPurchases.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        int currentYear = LocalDate.now().getYear();
+
+        LocalDate firstDay = LocalDate.ofYearDay(currentYear, 1);
+        LocalDate week1 = firstDay.plusDays(7);
+        Map<LocalDate, List<Purchase>> week1Purchases = datedPurchases.headMap(week1); // get datedPurchases from start to week1 exclude week1
+        Map<LocalDate, List<Purchase>> week2Purchases = datedPurchases.tailMap(week1); // get datedPurchases from week1 to end include week1
+
+//        System.out.println("-----------------------");
+//        week1Purchases.forEach((key, value) -> System.out.println(key + ": " + value));
+//        System.out.println("-----------------------");
+//        week2Purchases.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        displayStats(1, week1Purchases);
+        displayStats(2, week2Purchases);
 
     }
 
@@ -38,11 +73,27 @@ public class Main {
         }
 
 //        int day = purchases.size() + 1;
-        int day = new Random().nextInt(1, 5);
+        int day = new Random().nextInt(1, 15);
         String key = course.courseId() + "_" + existingStudent.getId();
         int year = LocalDate.now().getYear();
         Purchase purchase = new Purchase(course.courseId(),
                 existingStudent.getId(), price, year, day);
         purchases.put(key, purchase);
+    }
+
+    private static void displayStats(int period,
+                                     Map<LocalDate, List<Purchase>> periodData) {
+
+        System.out.println("-----------------------");
+        Map<String, Integer> weeklyCounts = new TreeMap<>();
+        periodData.forEach((key, value) -> {
+            System.out.println(key + ": " + value);
+            for (Purchase p : value) {
+                weeklyCounts.merge(p.courseId(), 1, (prev, current) -> {
+                    return prev + current;
+                });
+            }
+        });
+        System.out.printf("Week %d Purchases = %s%n", period, weeklyCounts);
     }
 }
