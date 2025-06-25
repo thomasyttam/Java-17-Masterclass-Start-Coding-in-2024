de9e6af08a92d07e9fd5671db4ee28e7a2b0b63d
Thomas Tam <tyttam@gmail.com>
2025-06-25 05:29:47 -0400

    FinalCollectionsChallenge

--- FinalCollectionsChallenge/src/dev/lpa/Cart.java
+++ FinalCollectionsChallenge/src/dev/lpa/Cart.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Cart {
@@ -11,4 +12,79 @@ public class Cart {
     private LocalDate cartDate;
     private CartType type;
     private Map<String, Integer> products;
+
+    public Cart(CartType type, int days) {
+        this.type = type;
+        id = lastId++;
+        cartDate = LocalDate.now().minusDays(days);
+        products = new HashMap<>();
+    }
+
+    public Cart(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getCartDate() {
+        return cartDate;
+    }
+
+    public void addItem(InventoryItem item, int qty) {
+
+        // if item exists, add the qty to old value
+        // if item not exists, add item with the qty.
+        // Integer::sum, add the old with new value (qty)
+        products.merge(item.getProduct().sku(), qty, Integer::sum);
+
+        if (!item.reserveItem(qty)) {
+            System.out.println("Ouch, something went wrong, could not add item");
+        }
+    }
+
+    public void removeItem(InventoryItem item, int qty) {
+
+        int current = products.get(item.getProduct().sku());
+        if (current <= qty) { // remove qty greater than number in carter
+            qty = current;
+            products.remove(item.getProduct().sku());
+            System.out.printf("Item [%s] removed from basket%n",
+                    item.getProduct().name());
+
+        } else {
+            products.merge(item.getProduct().sku(), qty,
+                    (oldVal, newVal) -> oldVal - newVal);
+            System.out.printf("%d [%s]s removed%n", qty, item.getProduct().name());
+        }
+        item.releaseItem(qty);
+    }
+
+    public void printSalesSlip(Map<String, InventoryItem> inventory) {
+
+        double total = 0;
+        System.out.println("------------------------------------");
+        System.out.println("Thank you for your sale: ");
+        for (var cartItem : products.entrySet()) {
+            var item = inventory.get(cartItem.getKey());
+            int qty = cartItem.getValue();
+            double itemizedPrice = (item.getPrice() * qty);
+            total += itemizedPrice;
+            System.out.printf("\t%s %-10s (%d)@ $%.2f = $%.2f%n",
+                    cartItem.getKey(), item.getProduct().name(), qty,
+                    item.getPrice(), itemizedPrice);
+        }
+        System.out.printf("Total Sale: $%.2f%n", total);
+        System.out.println("------------------------------------");
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "id=" + id +
+                ", cartDate=" + cartDate +
+                ", products=" + products +
+                '}';
+    }
 }
--- FinalCollectionsChallenge/src/dev/lpa/Store.java
+++ FinalCollectionsChallenge/src/dev/lpa/Store.java
@@ -1,8 +1,73 @@
 package dev.lpa;
 
+import java.util.*;
+
 public class Store {
+
+    private static Random random = new Random();
+    private Map<String, InventoryItem> inventory;
+    private NavigableSet<Cart> carts = new TreeSet<>(Comparator.comparing(Cart::getId));
+    private Map<Category, Map<String, InventoryItem>> aisleInventory;
 
     public static void main(String[] args) {
 
+        Store myStore = new Store();
+        myStore.stockStore();
+        myStore.listInventory();
+
     }
+
+    private void manageStoreCarts() {
+
+    }
+
+    private boolean checkOutCart(Cart cart) {
+
+        return false;
+    }
+
+    private void abandonCarts() {
+
+    }
+
+    private void listProductsByCategory() {
+
+    }
+
+    private void stockStore() {
+
+        inventory = new HashMap<>();
+        List<Product> products = new ArrayList<>(List.of(
+                new Product("A100","apple","local",Category.PRODUCE),
+                new Product("B100","banana","local",Category.PRODUCE),
+                new Product("P100","pear","local",Category.PRODUCE),
+                new Product("L103","lemon","local",Category.PRODUCE),
+                new Product("M201","milk","farm",Category.DAIRY),
+                new Product("Y001","yogurt","farm",Category.DAIRY),
+                new Product("C333","cheese","farm",Category.DAIRY),
+                new Product("R777","rice chex","Nabisco",Category.CEREAL),
+                new Product("G111","granola","Nat Valley",Category.CEREAL),
+                new Product("BB11","ground beef","butcher",Category.MEAT),
+                new Product("CC11","chicken","butcher",Category.MEAT),
+                new Product("BC11","bacon","butcher",Category.MEAT),
+                new Product("BC77","coke","coca cola",Category.BEVERAGE),
+                new Product("BC88","coffee","value",Category.BEVERAGE),
+                new Product("BC99","tea","herbal",Category.BEVERAGE)
+        ));
+
+        products.forEach(p -> inventory.put(p.sku(), new InventoryItem(p,
+                random.nextDouble(0, 1.25), 1000, 5)));
+
+    }
+
+    private void stockAisles() {
+
+    }
+
+    private void listInventory() {
+
+        System.out.println("--------------------------------------");
+        inventory.values().forEach(System.out::println);
+    }
+
 }
