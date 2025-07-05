546ce59f54e9eb5a83be4e5384cdf8c77c9b83e6
Thomas Tam <tyttam@gmail.com>
2025-07-05 08:42:08 -0400

    ImmutableChanllenges

---
+++ ImmutableChallenges/src/dev/lpa/Main.java
@@ -0,0 +1,14 @@
+package dev.lpa;
+
+import dev.bank.BankAccount;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount account =
+                new BankAccount(BankAccount.AccountType.CHECKING, 500);
+        System.out.println(account);
+
+    }
+}
