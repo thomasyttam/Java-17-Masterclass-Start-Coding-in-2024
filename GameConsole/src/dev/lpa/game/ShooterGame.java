e228c72c6aa138d102553f987d7e7f7c25bcc966
Thomas Tam <tyttam@gmail.com>
2025-07-17 01:45:48 -0400

    GameConsole

--- GameConsole/src/dev/lpa/game/ShooterGame.java
+++ GameConsole/src/dev/lpa/game/ShooterGame.java
@@ -1,9 +1,13 @@
 package dev.lpa.game;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ShooterGame extends Game<Shooter>{
 
+    public ShooterGame(String gameName) {
+        super(gameName);
+    }
 
     @Override
     public Shooter createNewPlayer(String name) {
@@ -12,6 +16,21 @@ public class ShooterGame extends Game<Shooter>{
 
     @Override
     public Map<Character, GameAction> getGameActions(int playerIndex) {
-        return null;
+        var map = new LinkedHashMap<>(Map.of(
+                'F',
+                new GameAction('F', "Find Prize", this::findPrize),
+                'S',
+                new GameAction('S', "Use your gun", this::useWeapon)
+        ));
+        map.putAll(getStandardActions()); // get the standard action from class Game
+        return map;
+    }
+
+    public boolean findPrize(int playerIndex) {
+        return getPlayer(playerIndex).findPrize();
+    }
+
+    public boolean useWeapon(int playerIndex) {
+        return getPlayer(playerIndex).useWeapon("pistol");
     }
 }
