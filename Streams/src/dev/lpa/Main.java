b0eb47313ddecfeee09629808c921eeef2b04684
Thomas Tam <tyttam@gmail.com>
2025-07-27 00:53:52 -0400

    Streams

--- Streams/src/dev/lpa/Main.java
+++ Streams/src/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
@@ -72,13 +73,83 @@ public class Main {
 //        }
 
         String[] strings = {"One", "Two", "Three"};
-        Arrays.stream(strings)
-                .sorted(Comparator.reverseOrder())
+//        Arrays.stream(strings)
+//                .sorted(Comparator.reverseOrder())
+//                .forEach(System.out::println);
+        var firstStream = Arrays.stream(strings)
+                .sorted(Comparator.reverseOrder());
+
+//        Stream.of("Six", "Five", "Four")
+//                .map(String::toUpperCase)
+//                .forEach(System.out::println);
+        var secondStream = Stream.of("Six", "Five", "Four")
+                .map(String::toUpperCase);
+
+        Stream.concat(secondStream, firstStream)
+                .map(s -> s.charAt(0) + " - " + s)
                 .forEach(System.out::println);
 
-        Stream.of("Six", "Five", "Four")
-                .map(String::toUpperCase)
+        Map<Character, int[]> myMap = new LinkedHashMap<>();
+        int bingoIndex = 1;
+        for (char c : "BINGO".toCharArray()) {
+            int[] numbers = new int[15];
+            int labelNo = bingoIndex;
+            Arrays.setAll(numbers, i -> i + labelNo);
+            myMap.put(c, numbers);
+            bingoIndex += 15;
+        }
+
+        myMap.entrySet()
+                .stream()
+                .map(e -> e.getKey() + " has range: " + e.getValue()[0] + " - " +
+                        e.getValue()[e.getValue().length - 1])
                 .forEach(System.out::println);
 
+        Random random = new Random();
+        Stream.generate(() -> random.nextInt(2)) // infinite number generate
+                .limit(10) // limit the output size.
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.iterate(1, n -> n + 1)  // IntStream: Stream special for Integer
+                .filter(Main::isPrime) // filter Prime number first
+                .limit(20) // limit Prime number to 20
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.iterate(1, n -> n + 1)  // IntStream: Stream special for Integer
+                .limit(100) // Get first 100 number
+                .filter(Main::isPrime) // limit prime number output
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.iterate(1, n -> n <= 100, n -> n + 1) // the overload version, the second one is the condition
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.range(1, 100) // the range use in the stream, start from 1 exclude 100
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.rangeClosed(1, 100) // the range use in the stream , start from 1 include 100
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+    }
+
+    public static boolean isPrime(int wholeNumber) {
+
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+
+        for (int divisor = 2; divisor < wholeNumber; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
