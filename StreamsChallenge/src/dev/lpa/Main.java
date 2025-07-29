809882ad01970a0122a36375dc8db0ff0a095a0c
Thomas Tam <tyttam@gmail.com>
2025-07-29 01:58:30 -0400

    StreamsIntermediate

--- StreamsChallenge/src/dev/lpa/Main.java
+++ StreamsChallenge/src/dev/lpa/Main.java
@@ -1,9 +1,12 @@
 package dev.lpa;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.Stream;
 
 public class Main {
+
+    static int counter = 0;
 
     public static void main(String[] args) {
 
@@ -27,7 +30,33 @@ public class Main {
 
         seed += 15;
         var streamG = Stream.of("G46", "G47", "G48", "G49", "G50",
-                "G51", "G52", "G53", "G54", "G55", "G56", "G57", "G58", "G59", "G60");
-        streamG.forEach(System.out::println);
+                "G51", "G52", "G53", "G54", "G55", "G56", "G57", "G58", "G59", "G60"); // create String directly
+//        streamG.forEach(System.out::println);
+
+        seed += 15;
+        int rSeed = seed;
+        var streamO = Stream.generate(Main::getCounter)
+                .limit(15)
+                .map(i -> "O" + (rSeed + i));
+//        streamO.forEach(System.out::println);
+
+        var streamBI = Stream.concat(streamB, streamI);
+        var streamNG = Stream.concat(streamN, streamG);
+        var streamBING = Stream.concat(streamBI, streamNG);
+        Stream.concat(streamBING, streamO)
+                .forEach(System.out::println);
+
+        System.out.println("-------------------------------------");
+//        Stream.generate(() -> new Random().nextInt(rSeed, rSeed + 14)) // change to 14, will run infinitely since cannot get 15 distinct number.
+        Stream.generate(() -> new Random().nextInt(rSeed, rSeed + 15))
+                .distinct()
+                .limit(15)
+                .map(i -> "O" + i)
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    private static int getCounter() {
+        return counter++;
     }
 }
