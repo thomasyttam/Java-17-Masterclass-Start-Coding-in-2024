1f17df685c8cf701cbb031919985f00bae058ccf
Thomas Tam <tyttam@gmail.com>
2025-08-17 10:19:43 -0400

    MoreTime

--- MoreTime/src/dev/lpa/Main.java
+++ MoreTime/src/dev/lpa/Main.java
@@ -1,10 +1,13 @@
 //        ZoneId bet = ZoneId.of("BET", ZoneId.SHORT_IDS);
 package dev.lpa;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.TimeZone;
 
@@ -52,5 +55,84 @@ public class Main {
 
         Instant instantNow = Instant.now(); // zero offset of time from UTC
         System.out.println(instantNow);
+
+        for (ZoneId z : List.of(
+                ZoneId.of("Australia/Sydney"),
+                ZoneId.of("Europe/Paris"),
+                ZoneId.of("America/New_York"))) {
+            DateTimeFormatter zoneFormat = DateTimeFormatter.ofPattern("z:zzzz");
+            System.out.println(z);
+            System.out.println("\t" + instantNow.atZone(z).format(zoneFormat));
+            System.out.println("\t" + z.getRules().getDaylightSavings(instantNow));
+            System.out.println("\t" + z.getRules().isDaylightSavings(instantNow));
+        }
+
+        Instant dobInstant = Instant.parse("2020-01-01T08:01:00Z");
+
+        LocalDateTime dob =
+                LocalDateTime.ofInstant(dobInstant, ZoneId.systemDefault());
+        System.out.println("Your kid's birthdate, UTC time = " + dobInstant);
+        System.out.println("Your kid's birthdate, LA time = " + dob.format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
+        ZonedDateTime dobSydney = ZonedDateTime.ofInstant(dobInstant,
+                ZoneId.of("Australia/Sydney"));
+        System.out.println("Your kid's birthdate, Sydney Time = " + dobSydney.format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
+
+        ZonedDateTime dobHere = dobSydney.withZoneSameInstant(ZoneId.systemDefault());
+        System.out.println("Your kid's birthdate, Here Time = " + dobHere.format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
+        ZonedDateTime firstOfMonth = ZonedDateTime.now()
+                .with(TemporalAdjusters.firstDayOfNextMonth());
+        System.out.printf("First of next Month = %tD %n", firstOfMonth);
+
+        Period timePast = Period.between(LocalDate.EPOCH, dob.toLocalDate());
+        System.out.println(timePast); // P -> period, Y -> year
+
+        Duration timeSince =
+                Duration.between(Instant.EPOCH, dob.toInstant(ZoneOffset.UTC));
+        System.out.println(timeSince); // PT -> period of time, H -> hours, M -> minutes
+
+        // dob2 This child will be born 2 years,4 months, 4 days, 7 hours, 14 minutes, and 37 seconds later than the first child.
+        LocalDateTime dob2 = dob.plusYears(2).plusMonths(4)
+                .plusDays(4).plusHours(7).plusMinutes(14)
+                .plusSeconds(37);
+
+        System.out.println("Your 2nd kid's birthdate, Here Time = " + dob2.format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
+        Period timePast2 = Period.between(LocalDate.EPOCH, dob2.toLocalDate());
+        System.out.println(timePast2);
+
+        Duration timeSince2 =
+                Duration.between(Instant.EPOCH, dob2.toInstant(ZoneOffset.UTC));
+        System.out.println(timeSince2);
+
+        for (ChronoUnit u : ChronoUnit.values()) {
+            if (u.isSupportedBy(LocalDate.EPOCH)){
+                long val = u.between(LocalDate.EPOCH,
+                        dob2.toLocalDate());
+                System.out.println(u + " past = " + val);
+            } else {
+                System.out.println("-- Not supported: " + u);
+            }
+        }
+
+        LocalDateTime ldt = LocalDateTime.ofInstant(Instant.EPOCH,
+                ZoneOffset.UTC);
+
+        for (ChronoUnit u : ChronoUnit.values()) {
+            if (u.isSupportedBy(ldt)){
+                long val = u.between(ldt,
+                        dob2);
+                System.out.println(u + " past = " + val);
+            } else {
+                System.out.println("-- Not supported: " + u);
+            }
+        }
+
     }
 }
