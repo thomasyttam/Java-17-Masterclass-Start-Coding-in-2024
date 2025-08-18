04260ec5a4bfb6fa12790b4829a01263704de23a
Thomas Tam <tyttam@gmail.com>
2025-08-18 06:16:49 -0400

    DateTimeLocalizationProject

---
+++ DateTimeLocalizationProject/src/dev/lpa/Main.java
@@ -0,0 +1,60 @@
+package dev.lpa;
+
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.zone.ZoneRules;
+import java.util.Locale;
+
+import static java.time.format.DateTimeFormatter.*;
+
+public class Main {
+
+    private record Employee (String name, Locale locale, ZoneId zone) {
+
+        public Employee(String name, String locale, String zone) {
+            this(name, Locale.forLanguageTag(locale), ZoneId.of(zone));
+        }
+
+        public Employee(String name, Locale locale, String zone) {
+            this(name, locale, ZoneId.of(zone));
+        }
+
+        String getDateInfo(ZonedDateTime zdt, DateTimeFormatter dtf) {
+            return "%s [%s] : %s".formatted(name, zone,
+                    zdt.format(dtf.localizedBy(locale)));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee jane = new Employee("Jane", Locale.US, "America/New_York");
+        Employee joe = new Employee("Joe", "en-AU", "Australia/Sydney");
+
+        ZoneRules joesRules = joe.zone.getRules();
+        ZoneRules janesRules = jane.zone.getRules();
+        System.out.println(jane + " " + janesRules);
+        System.out.println(joe + " " + joesRules);
+
+        ZonedDateTime janeNow = ZonedDateTime.now(jane.zone);
+        System.out.println(janeNow);
+        ZonedDateTime joeNow = ZonedDateTime.of(janeNow.toLocalDateTime(), joe.zone);
+        System.out.println(joeNow);
+        long hoursBetween = Duration.between(joeNow, janeNow).toHours();
+        long minutesBetween = Duration.between(joeNow, janeNow).toMinutesPart();
+        System.out.println("Joe is " + Math.abs(hoursBetween) + " hours " +
+                Math.abs(minutesBetween) + " minutes " +
+                ((hoursBetween < 0) ? "behind" : "ahead"));
+
+        System.out.println("Joe in daylight savings? " +
+                joesRules.isDaylightSavings(joeNow.toInstant()) + " " +
+                joesRules.getDaylightSavings(joeNow.toInstant()) + ": " +
+                joeNow.format(ofPattern("zzzz z")));
+
+        System.out.println("Jane in daylight savings? " +
+                janesRules.isDaylightSavings(janeNow.toInstant()) + " " +
+                janesRules.getDaylightSavings(janeNow.toInstant()) + ": " +
+                janeNow.format(ofPattern("zzzz z")));
+    }
+}
