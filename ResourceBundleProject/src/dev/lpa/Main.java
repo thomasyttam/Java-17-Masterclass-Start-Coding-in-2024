c9161b4e050034fd782f38fb5a8645214eba6215
Thomas Tam <tyttam@gmail.com>
2025-08-20 11:22:38 -0400

    ResourceBundleProject

--- ResourceBundleProject/src/dev/lpa/Main.java
+++ ResourceBundleProject/src/dev/lpa/Main.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import javax.swing.*;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -16,8 +17,24 @@ public class Main {
 //            System.out.println(rb.getBaseBundleName());
 //            System.out.println(rb.keySet());
 
-            System.out.printf("%s %s!%n",
+//            System.out.printf("%s %s!%n",
+//                    rb.getString("hello"), rb.getString("world")); // key is case-sensitive
+
+            String message = "%s %s!%n".formatted(
                     rb.getString("hello"), rb.getString("world")); // key is case-sensitive
+
+            ResourceBundle ui = ResourceBundle.getBundle("UIComponents", l);
+
+            JOptionPane.showOptionDialog(null, // use current window
+                    message,
+                    ui.getString("first.title"),
+                    JOptionPane.DEFAULT_OPTION, // dialog with ok button
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+//                    null,
+//                    new Object[]{ui.getString("btn.ok")}, //get button in different language
+                    new Object[]{rb.getString("yes"), rb.getString("no")},
+                    null);
         }
 
     }
