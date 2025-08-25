86ff4951a8673ada1bb0f9853b2930f02847317e
Thomas Tam <tyttam@gmail.com>
2025-08-25 03:47:54 -0400

    FileExceptions

---
+++ FIleExceptions/src/dev/lpa/Main.java
@@ -0,0 +1,43 @@
+package dev.lpa;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String filename = "testing.csv";
+        Path path = Paths.get(filename);
+
+        testFile(filename);
+
+        File file = new File(filename);
+
+        if (!file.exists()) {
+            System.out.println("I can't run unless this file exists");
+            System.out.println("Quitting Application, go figure it out");
+            return;
+        }
+        System.out.println("I'm good to go.");
+
+    }
+
+    private static void testFile(String filename) {
+
+        Path path = Paths.get(filename);
+        try {
+            List<String> lines = Files.readAllLines(path);
+        } catch (IOException e) {
+//            int i = 1/0;
+            throw new RuntimeException(e);
+        } finally { // finally block still executes when an exception occured in try or cathc block
+            System.out.println("Maybe I'd log something either way...");
+        }
+        System.out.println("File exists and able to use as a resource");
+    }
+}
