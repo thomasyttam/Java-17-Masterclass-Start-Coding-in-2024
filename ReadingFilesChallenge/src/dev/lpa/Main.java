47113df6053b7e3c11be4a4395f2f9256bbe92b6
Thomas Tam <tyttam@gmail.com>
2025-09-05 05:36:45 -0400

    WritingFiles

--- ReadingFilesChallenge/src/dev/lpa/Main.java
+++ ReadingFilesChallenge/src/dev/lpa/Main.java
@@ -3,24 +3,85 @@ package dev.lpa;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try(BufferedReader br = new BufferedReader(
-                new FileReader("article.txt"))) {
+//                new FileReader("article.txt"))) {
+                new FileReader("bigben.txt"))) {
+
 
 //            System.out.printf("%d lines in files%n", br.lines().count());
             Pattern pattern = Pattern.compile("\\p{javaWhitespace}+");
-            System.out.printf("%,d words in file%n",
-                    br.lines()
-                            .flatMap(pattern::splitAsStream)
-//                            .flatMap(l -> Arrays.stream(l.split(pattern.toString())))
-                            .count());
+//            System.out.printf("%,d words in file%n",
+//                    br.lines()
+//                            .flatMap(pattern::splitAsStream)
+////                            .flatMap(l -> Arrays.stream(l.split(pattern.toString())))
+//                            .count());
+
+//            System.out.printf("%,d words in file%n",
+//                    br.lines()
+//                            .mapToLong(l -> l.split(pattern.toString()).length)
+//                            .sum());
+
+            List<String> excluded = List.of(
+                    "grand",
+                    "canyon",
+                    "retrieved",
+                    "archived",
+                    "service",
+                    "original");
+            var result = br.lines()
+                    .flatMap(pattern::splitAsStream)
+                    .map(w -> w.replaceAll("\\p{Punct}", ""))
+                    .filter(w -> w.length() > 4) // filter the word greater than 4
+                    .map(String::toLowerCase)
+                    .filter(w -> !excluded.contains(w)) // filter out the list excluded
+                    .collect(Collectors.groupingBy(w -> w, Collectors.counting())); // count w, return w and counts
+
+            result.entrySet().stream()
+                    .sorted(Comparator.comparing(Map.Entry::getValue,
+                            Comparator.reverseOrder()))
+                    .limit(10) // get the top 10
+                    .forEach(e -> System.out.println(
+                            e.getKey() + " - " + e.getValue() + " times"));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        System.out.println("--------------------");
+        String input = null;
+        try {
+            input = Files.readString(Path.of("bigben.txt"));
+            input = input.replaceAll("\\p{Punct}", "");
+
+            Pattern pattern = Pattern.compile("\\w+");
+            Matcher matcher = pattern.matcher(input);
+            Map<String, Long> results = new HashMap<>();
+            while (matcher.find()) {
+                String word = matcher.group().toLowerCase();
+                if (word.length() > 4) {
+                    results.merge(word, 1L, (o, n) -> o += n);
+                }
+            }
+
+            var sortedEntries = new ArrayList<>(results.entrySet());
+            sortedEntries.sort(Comparator.comparing
+                    (Map.Entry::getValue, Comparator.reverseOrder()));
+            for (int i = 0; i < Math.min(10, sortedEntries.size()); i++) {
+                var entry = sortedEntries.get(i);
+                System.out.println(entry.getKey() + " - " + entry.getValue() + " times");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
