8114b26bf3885d03e737179e246593117082515f
Thomas Tam <tyttam@gmail.com>
2025-09-17 05:27:16 -0400

    DirectoryFileChallenge

--- DirectoryFileChallenge/src/dev/lpa/Main.java
+++ DirectoryFileChallenge/src/dev/lpa/Main.java
@@ -1,8 +1,19 @@
 package dev.lpa;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Path deepestFolders = Path.of("public", "assets", "icons");
+
+        try {
+            Files.createDirectories(deepestFolders);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
