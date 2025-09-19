105eca503082693ed2ba44150f9d5aff4c0ca0ff
Thomas Tam <tyttam@gmail.com>
2025-09-19 05:32:44 -0400

    RandomAccess

--- RandomAccess/src/dev/lpa/Main.java
+++ RandomAccess/src/dev/lpa/Main.java
@@ -1,7 +1,10 @@
 package dev.lpa;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,7 +13,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BuildStudentData.build("studentData");
+//        BuildStudentData.build("studentData");
+        BuildStudentData.build("student");
 
+        try (RandomAccessFile ra =
+                     new RandomAccessFile("student.dat", "r")) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a Student Id or 0 to quit");
+            while (scanner.hasNext()) {
+                long studentId = Long.parseLong(scanner.nextLine());
+                if (studentId < 1) {
+                    break;
+                }
+                ra.seek(indexedIds.get(studentId));
+                String targetedRecord = ra.readUTF();
+                System.out.println(targetedRecord);
+                System.out.println("Enter another Student Id or 0 to quit");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void loadIndex(RandomAccessFile ra, int indexPosition) {
+        try {
+            ra.seek(indexPosition);
+            recordsInFile = ra.readInt();
+            System.out.println(recordsInFile);
+            for (int i = 0; i < recordsInFile; i++) {
+                indexedIds.put(ra.readLong(), ra.readLong());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
