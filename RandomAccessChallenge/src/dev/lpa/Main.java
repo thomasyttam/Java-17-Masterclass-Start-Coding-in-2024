efc7d05654b1529dca5331e1c526f8c3f741c669
Thomas Tam <tyttam@gmail.com>
2025-09-21 11:55:45 -0400

    RandomAccessChallenge

---
+++ RandomAccessChallenge/src/dev/lpa/Main.java
@@ -0,0 +1,12 @@
+package dev.lpa;
+
+record Employee(int employeeId, String firstName, String lastName, double salary) {
+
+};
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
