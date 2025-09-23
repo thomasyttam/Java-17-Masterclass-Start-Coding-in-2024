f16c807481212b0eafff13781816c02e4ea56f65
Thomas Tam <tyttam@gmail.com>
2025-09-23 06:07:50 -0400

    RandomAccessChallenge

--- RandomAccessChallenge/src/dev/lpa/Main.java
+++ RandomAccessChallenge/src/dev/lpa/Main.java
@@ -2,8 +2,7 @@ package dev.lpa;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 record Employee(int employeeId, String firstName, String lastName, double salary) {
 
@@ -29,6 +28,37 @@ public class Main {
     }
     public static void main(String[] args) {
 
+        try (RandomAccessFile ra =
+                     new RandomAccessFile("employees.dat", "rw")) {
+            Scanner scanner = new Scanner(System.in);
+            List<Integer> ids = new ArrayList<>(indexedIds.keySet());
+            Collections.sort(ids);
+
+            while (true) {
+                System.out.println(ids);
+                System.out.println("Enter an Employee Id or 0 to quit");
+                if (!scanner.hasNext()) break;
+                int employeeId = Integer.parseInt(scanner.nextLine());
+                if (employeeId < 1) {
+                    break;
+                }
+                if (!ids.contains(employeeId)) {
+                    continue;
+                }
+                Employee e = readRecord(ra, employeeId);
+                System.out.println("Enter new salary, nothing if no change:");
+                try {
+                    double salary = Double.parseDouble(scanner.nextLine());
+                    ra.seek(indexedIds.get(employeeId) + 4);
+                    ra.writeDouble(salary);
+                    readRecord(ra, employeeId);
+                } catch (NumberFormatException ignore) {
+                    // If the entered input is not a valid number, I'll ignore it.
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static Employee readRecord(RandomAccessFile ra, int employeeId)
