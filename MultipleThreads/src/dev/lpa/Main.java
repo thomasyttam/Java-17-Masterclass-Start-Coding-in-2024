e064b430ace5a728832d372aeed1022b07b26aab
Thomas Tam <tyttam@gmail.com>
2025-09-30 03:38:26 -0400

    MultipleThreads

--- MultipleThreads/src/dev/lpa/Main.java
+++ MultipleThreads/src/dev/lpa/Main.java
@@ -1,8 +1,62 @@
 package dev.lpa;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
 
+//        StopWatch stopWatch = new StopWatch(TimeUnit.SECONDS);
+//        Thread green = new Thread(stopWatch::countDown, ThreadColor.ANSI_GREEN.name());
+//        Thread purple = new Thread(() -> stopWatch.countDown(7),
+//                ThreadColor.ANSI_PURPLE.name());
+//        Thread red = new Thread(stopWatch::countDown, ThreadColor.ANSI_RED.name());
+        // create 3 different StopWatch
+        StopWatch greenWatch = new StopWatch(TimeUnit.SECONDS);
+        StopWatch purpleWatch = new StopWatch(TimeUnit.SECONDS);
+        StopWatch redWatch = new StopWatch(TimeUnit.SECONDS);
+        Thread green = new Thread(greenWatch::countDown, ThreadColor.ANSI_GREEN.name());
+        Thread purple = new Thread(() -> purpleWatch.countDown(7),
+                ThreadColor.ANSI_PURPLE.name());
+        Thread red = new Thread(redWatch::countDown, ThreadColor.ANSI_RED.name());
+        green.start();
+        purple.start();
+        red.start();
+    }
+}
+
+class StopWatch { // package-private (default, no modifier) – visible only inside the same package.
+
+    private TimeUnit timeUnit;
+    private int i;
+
+    public StopWatch(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    public void countDown() {
+        countDown(5);
+    }
+
+    public void countDown(int unitCount) {
+
+        String threadName = Thread.currentThread().getName();
+
+        ThreadColor threadColor = ThreadColor.ANSI_RESET;
+        try {
+            threadColor = ThreadColor.valueOf(threadName);
+        } catch (IllegalArgumentException ignore) {
+            // User may pass a bad color name, Will just ignore this error.
+        }
+        String color = threadColor.color();
+//        for (int i = unitCount; i > 0; i--) {
+        for (i = unitCount; i > 0; i--) {
+            try {
+                timeUnit.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("%s%s Thread : i = %d%n", color, threadName, i);
+        }
     }
 }
