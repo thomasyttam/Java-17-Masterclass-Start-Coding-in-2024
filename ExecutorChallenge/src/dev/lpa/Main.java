7e6d21fa23d54820864d48552cd37d1ded00a9a6
Thomas Tam <tyttam@gmail.com>
2025-10-13 07:10:46 -0400

    ExecutorChallenge

---
+++ ExecutorChallenge/src/dev/lpa/Main.java
@@ -0,0 +1,46 @@
+package dev.lpa;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+record Order(long orderId, String item, int qty) {
+};
+
+public class Main {
+
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+
+        ShoeWarehouse warehouse = new ShoeWarehouse();
+
+        ExecutorService orderingService = Executors.newCachedThreadPool();
+        
+//        Thread producerThread = new Thread(() -> {
+//            for (int j = 0; j < 10; j++) {
+//                warehouse.receiveOrder(new Order(
+//                        random.nextLong(1000000, 9999999),
+//                        ShoeWarehouse.PRODUCT_LIST[random.nextInt(0, 5)],
+//                        random.nextInt(1, 4)));
+//            }
+//        });
+//        producerThread.start();
+
+//        for (int i = 0; i < 2; i++) {
+//            Thread consumerThread = new Thread(() -> {
+//                for (int j = 0; j < 5; j++) {
+//                    Order item = warehouse.fulfillOrder();
+//                }
+//            });
+//            consumerThread.start();
+//        }
+
+    }
+    private static Order generateOrder() {
+        return new Order(
+                random.nextLong(1000000, 9999999),
+                ShoeWarehouse.PRODUCT_LIST[random.nextInt(0, 5)],
+                random.nextInt(1, 4));
+    }
+}
