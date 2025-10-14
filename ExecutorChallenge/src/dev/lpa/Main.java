9b640e3a013481911da7c8b0b18a5f684b5106b4
Thomas Tam <tyttam@gmail.com>
2025-10-14 05:55:05 -0400

    ExecutorChallenge

--- ExecutorChallenge/src/dev/lpa/Main.java
+++ ExecutorChallenge/src/dev/lpa/Main.java
@@ -1,8 +1,12 @@
 package dev.lpa;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 record Order(long orderId, String item, int qty) {
 };
@@ -16,8 +20,35 @@ public class Main {
         ShoeWarehouse warehouse = new ShoeWarehouse();
 
         ExecutorService orderingService = Executors.newCachedThreadPool();
-        
-//        Thread producerThread = new Thread(() -> {
+
+        Callable<Order> orderingTask = () -> {
+            {
+                Order newOrder = generateOrder();
+                try {
+                    Thread.sleep(random.nextInt(500, 5000));
+                    warehouse.receiveOrder(newOrder);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return newOrder;
+            }
+        };
+
+        List<Callable<Order>> tasks = Collections.nCopies(15, orderingTask);
+        try {
+            orderingService.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        orderingService.shutdown();
+        try {
+            orderingService.awaitTermination(6, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+//            Thread producerThread = new Thread(() -> {
 //            for (int j = 0; j < 10; j++) {
 //                warehouse.receiveOrder(new Order(
 //                        random.nextLong(1000000, 9999999),
