315e3f953a9fb66c980a0bfed95c7d0feb52d7bf
Thomas Tam <tyttam@gmail.com>
2025-10-16 04:00:32 -0400

    SchedulingTasks

---
+++ SchedulingTasks/src/dev/lpa/Main.java
@@ -0,0 +1,29 @@
+package dev.lpa;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var dtf = DateTimeFormatter.ofLocalizedDateTime(
+                FormatStyle.MEDIUM,
+                FormatStyle.LONG
+        );
+
+        Callable<ZonedDateTime> waitThenDoIt = () -> {
+            ZonedDateTime zdt = null;
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                zdt = ZonedDateTime.now();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return zdt;
+        };
+    }
+}
