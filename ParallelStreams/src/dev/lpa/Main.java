35a9fa32e3c1ec22625c663bba67a88d2716a390
Thomas Tam <tyttam@gmail.com>
2025-10-19 08:30:56 -0400

    ParallelStreams

--- ParallelStreams/src/dev/lpa/Main.java
+++ ParallelStreams/src/dev/lpa/Main.java
@@ -1,8 +1,13 @@
 package dev.lpa;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        int numbersLength = 100_000;
+        long[] numbers = new Random().longs(numbersLength,
+                1, numbersLength).toArray();
     }
 }
