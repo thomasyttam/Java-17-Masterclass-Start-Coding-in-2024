ef31a159230313acbe60154133d41065558ebd25
Thomas Tam <tyttam@gmail.com>
2025-10-21 05:08:07 -0400

    ParallelStreamsAndMore

--- ParallelStreamsAndMore/src/dev/lpa/Main.java
+++ ParallelStreamsAndMore/src/dev/lpa/Main.java
@@ -1,8 +1,51 @@
 package dev.lpa;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+import java.util.stream.Stream;
+
+record Person(String firstName, String lastName, int age) {
+
+    private final static String[] firsts =
+            {"Able", "Bob", "Charlie", "Donna", "Eve", "Fred"};
+    private final static String[] lasts =
+            {"Norton", "OHara", "Petersen", "Quincy", "Richardson", "Smith"};
+
+    private final static Random random = new Random();
+
+    public Person() {
+        this(firsts[random.nextInt(firsts.length)],
+                lasts[random.nextInt(lasts.length)],
+                random.nextInt(18, 100));
+    }
+
+    @Override
+    public String toString() {
+        return "%s, %s (%d)".formatted(lastName, firstName, age);
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        var persons = Stream.generate(Person::new)
+                .limit(10)
+                .sorted(Comparator.comparing(Person::lastName))
+                .toArray();
+
+        for (var person : persons) {
+            System.out.println(person);
+        }
+        System.out.println("-----------------------------");
+
+//        Stream.generate(Person::new)
+        Arrays.stream(persons)
+                .limit(10)
+                .parallel()
+//                .sorted(Comparator.comparing(Person::lastName))
+//                .forEach(System.out::println);
+                .forEachOrdered(System.out::println);
     }
 }
