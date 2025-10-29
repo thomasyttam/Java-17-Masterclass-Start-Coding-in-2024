d52fbd00bd209b03bbec3d9bdbf49be171f96e57
Thomas Tam <tyttam@gmail.com>
2025-10-29 11:19:22 -0400

    ParallelStreamsAndMore

--- ParallelStreamsAndMore/src/dev/lpa/VisitorList.java
+++ ParallelStreamsAndMore/src/dev/lpa/VisitorList.java
@@ -29,7 +29,7 @@ public class VisitorList {
 
         Runnable producer = () -> {
             Person visitor = new Person();
-            System.out.println("Adding " + visitor);
+            System.out.println("Queueing " + visitor);
             boolean queued = false;
             try {
                 queued = newVisitors.offer(visitor, 5, TimeUnit.SECONDS);
@@ -44,7 +44,7 @@ public class VisitorList {
 //                System.out.println("Illegal State Exception!");
 //            }
             if (queued) {
-                System.out.println(newVisitors);
+//                System.out.println(newVisitors);
             } else {
                 System.out.println("Queue is Full, cannot add " + visitor);
                 System.out.println("Draining Queue and writing data to file");
@@ -63,15 +63,36 @@ public class VisitorList {
             }
         };
 
+        Runnable consumer = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " Polling queue " + newVisitors.size());
+            Person visitor = newVisitors.poll();
+
+            if (visitor != null) {
+                System.out.println(threadName + " " + visitor);
+                if (!masterList.contains(visitor)) {
+                    masterList.add(visitor);
+                    System.out.println("--> New Visitor gets Coupon!: " + visitor);
+                }
+            }
+            System.out.println(threadName + " done " + newVisitors.size());
+        };
 
         ScheduledExecutorService producerExecutor = // execute producer
                 Executors.newSingleThreadScheduledExecutor();
         producerExecutor.scheduleWithFixedDelay(producer, 0, 1,
                 TimeUnit.SECONDS);
 
+        ScheduledExecutorService consumerPool =
+                Executors.newScheduledThreadPool(3);
+        for (int i = 0; i < 3; i++) {
+            consumerPool.scheduleAtFixedRate(consumer, 6, 1,
+                    TimeUnit.SECONDS);
+        }
+
         while (true) {
             try {
-                if (!producerExecutor.awaitTermination(20, TimeUnit.SECONDS))
+                if (!producerExecutor.awaitTermination(10, TimeUnit.SECONDS))
                     break;
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
@@ -79,5 +100,16 @@ public class VisitorList {
         }
 
         producerExecutor.shutdown();
+
+        while (true) {
+            try {
+                if (!consumerPool.awaitTermination(3, TimeUnit.SECONDS))
+                    break;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        consumerPool.shutdown();
     }
 }
