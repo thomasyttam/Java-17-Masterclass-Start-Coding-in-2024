b0c5f604c5e5020083bc5875495a806dab06bebf
Thomas Tam <tyttam@gmail.com>
2025-11-04 05:15:37 -0500

    ThreadProblems

--- ThreadProblems/src/dev/lpa/Maze.java
+++ ThreadProblems/src/dev/lpa/Maze.java
@@ -1,4 +1,15 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public class Maze {
+
+    public static final int MAZE_SIZE = 4;
+    private final String[][] cells = new String[MAZE_SIZE][MAZE_SIZE];
+
+    public Maze() {
+        for (var row : cells) {
+            Arrays.fill(row, "");
+        }
+    }
 }
