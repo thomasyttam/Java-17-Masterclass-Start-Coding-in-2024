fa4cea5c8dd1ad29008c5aea710829c9ebcbee85
Thomas Tam <tyttam@gmail.com>
2025-11-09 13:40:29 -0500

    ConcurrencyExtras

--- ConcurrencyExtras/src/dev/lpa/Main.java
+++ ConcurrencyExtras/src/dev/lpa/Main.java
@@ -1,7 +1,9 @@
 package dev.lpa;
 
 import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.Executors;
 
 record Student(String name, int enrolledYear, int studentId)
         implements Comparable<Student> {
@@ -33,6 +35,17 @@ public class Main {
             new ConcurrentSkipListSet<>();
 
     public static void main(String[] args) {
+
+        StudentId idGenerator = new StudentId();
+        Callable<Student> studentMaker = () -> {
+            int studentId = idGenerator.getNextId();
+            Student student = new Student("Tim " + studentId,
+                    random.nextInt(2018, 2025), studentId);
+            studentSet.add(student);
+            return student;
+        };
+
+        var executor = Executors.newCachedThreadPool();
 
     }
 }
