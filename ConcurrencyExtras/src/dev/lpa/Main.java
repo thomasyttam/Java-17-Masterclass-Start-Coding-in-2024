a8bbf5cd2d4ea93264bfb2364bf7863b0d683fcd
Thomas Tam <tyttam@gmail.com>
2025-11-12 05:33:36 -0500

    ConcurrencyExtras

--- ConcurrencyExtras/src/dev/lpa/Main.java
+++ ConcurrencyExtras/src/dev/lpa/Main.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 record Student(String name, int enrolledYear, int studentId)
         implements Comparable<Student> {
@@ -25,6 +26,19 @@ class StudentId {
 
     public synchronized int getNextId() {
         return ++id;
+    }
+}
+
+class AtomicStudentId {
+
+    private final AtomicInteger nextId = new AtomicInteger(0);
+
+    public int getId() {
+        return nextId.get();
+    }
+
+    public int getNextId() {
+        return nextId.incrementAndGet();
     }
 }
 
