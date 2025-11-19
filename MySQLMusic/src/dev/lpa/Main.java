501e53398ee682e459b0f9d922062cff2db4abc5
Thomas Tam <tyttam@gmail.com>
2025-11-19 06:36:52 -0500

    MySQLMusic

--- MySQLMusic/src/dev/lpa/Main.java
+++ MySQLMusic/src/dev/lpa/Main.java
@@ -1,8 +1,20 @@
 package dev.lpa;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        private final static String CONN_STRING = "jdbc:mysql://localhost:3306/music";
+
+        String username = JOptionPane.showInputDialog(
+                null, "Enter DB Username");
+
+        JPasswordField pf = new JPasswordField();
+        int okCxl = JOptionPane.showConfirmDialog(null, pf,
+                "Enter DB Password", JOptionPane.OK_CANCEL_OPTION);
+        final char[] password =
+                (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
     }
 }
