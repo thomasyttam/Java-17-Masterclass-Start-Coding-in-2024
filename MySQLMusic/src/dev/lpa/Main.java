a178db836444cd5be8528a1158c4e8dcb29c5b80
Thomas Tam <tyttam@gmail.com>
2025-11-20 05:45:52 -0500

    MySQLMusic

--- MySQLMusic/src/dev/lpa/Main.java
+++ MySQLMusic/src/dev/lpa/Main.java
@@ -1,12 +1,16 @@
 package dev.lpa;
 
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private final static String CONN_STRING = "jdbc:mysql://localhost:3306/music";
 
-        private final static String CONN_STRING = "jdbc:mysql://localhost:3306/music";
+    public static void main(String[] args) {
 
         String username = JOptionPane.showInputDialog(
                 null, "Enter DB Username");
@@ -16,5 +20,14 @@ public class Main {
                 "Enter DB Password", JOptionPane.OK_CANCEL_OPTION);
         final char[] password =
                 (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
+
+        try (Connection connection = DriverManager.getConnection(
+                CONN_STRING, username, String.valueOf(password))) {
+
+            System.out.println("Success!! Connection made to the music database");
+            Arrays.fill(password, ' ');
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
