2d41350463c735dbb7e8ec7d1f52ca358e83cbc5
Thomas Tam <tyttam@gmail.com>
2025-11-23 10:28:52 -0500

    QueryMusic

--- QueryMusic/src/dev/lpa/Main.java
+++ QueryMusic/src/dev/lpa/Main.java
@@ -1,8 +1,39 @@
 package dev.lpa;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Properties props = new Properties();
+        try {
+            props.load(Files.newInputStream(Path.of("music.properties"),
+                    StandardOpenOption.READ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        var dataSource = new MysqlDataSource();
+        dataSource.setServerName(props.getProperty("serverName"));
+        dataSource.setPort(Integer.parseInt(props.getProperty("port")));
+        dataSource.setDatabaseName(props.getProperty("databaseName"));
+
+        try (var connection = dataSource.getConnection(
+                props.getProperty("user"),
+                System.getenv("MYSQL_PASS"))
+        ) {
+            System.out.println("Success!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
