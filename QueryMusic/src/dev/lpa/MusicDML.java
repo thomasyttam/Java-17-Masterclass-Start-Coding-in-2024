b36dc57f5d8781ba03399cfb9c62db6e822728de
Thomas Tam <tyttam@gmail.com>
2025-11-30 11:27:17 -0500

    QueryMusic

--- QueryMusic/src/dev/lpa/MusicDML.java
+++ QueryMusic/src/dev/lpa/MusicDML.java
@@ -1,9 +1,6 @@
 package dev.lpa;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MusicDML {
 
@@ -24,10 +21,49 @@ public class MusicDML {
             String artist = "Elf";
             String query = "SELECT * from artists WHERE artist_name='%s'"
                     .formatted(artist);
-            boolean result = statement.execute(query);
+            boolean result = statement.execute(query); // result always true
             System.out.println("result = " + result);
+            var rs = statement.executeQuery(query);
+            boolean found = (rs != null && rs.next());
+            System.out.println("Artist was " + (found ? "found" : "not found"));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static boolean printRecords(ResultSet resultSet) throws SQLException {
+
+        boolean foundData = false;
+        var meta = resultSet.getMetaData();
+
+        System.out.println("===================");
+
+        for (int i = 1; i <= meta.getColumnCount(); i++) {
+            System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+        }
+        System.out.println();
+
+        while (resultSet.next()) {
+
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                System.out.printf("%-15s", resultSet.getString(i));
+            }
+            System.out.println();
+            foundData = true;
+        }
+        return foundData;
+    }
+
+    private static boolean executeSelect(Statement statement, String table,
+                                         String columnName, String columnValue)
+            throws SQLException {
+
+        String query = "SELECT * FROM %s WHERE %s='%s'"
+                .formatted(table, columnName, columnValue);
+        var rs = statement.executeQuery(query);
+        if (rs != null) {
+            return printRecords(rs);
+        }
+        return false;
     }
 }
