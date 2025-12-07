0c58421dc8b4d1b7269e687b4abe4e699315c0e4
Thomas Tam <tyttam@gmail.com>
2025-12-07 11:19:05 -0500

    QueryMusic

--- QueryMusic/src/dev/lpa/MusicDML.java
+++ QueryMusic/src/dev/lpa/MusicDML.java
@@ -196,7 +196,20 @@ public class MusicDML {
             throws SQLException {
 
         System.out.println("AUTOCOMMIT = " + conn.getAutoCommit());
-
+        String deleteSongs = """
+                DELETE FROM music.songs WHERE album_id = 
+                (SELECT ALBUM_ID from music.albums WHERE album_name = '%s')"""
+                .formatted(albumName);
+        int deletedSongs = statement.executeUpdate(deleteSongs);
+        System.out.printf("Deleted %d rows from music.songs%n", deletedSongs);
+        String deleteAlbums = "DELETE FROM music.albums WHERE album_name='%s'"
+                .formatted(albumName);
+        int deletedAlbums = statement.executeUpdate(deleteAlbums);
+        System.out.printf("Deleted %d rows from music.album%n", deletedAlbums);
+        String deleteArtist = "DELETE FROM music.artists WHERE artist_name='%s'"
+                .formatted(artistName);
+        int deletedArtists = statement.executeUpdate(deleteArtist);
+        System.out.printf("Deleted %d rows from music.artists%n", deletedArtists);
     }
 
 }
