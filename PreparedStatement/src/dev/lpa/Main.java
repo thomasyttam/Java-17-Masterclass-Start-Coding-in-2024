0a45c951db16f691fb2f618421fb772259e995a5
Thomas Tam <tyttam@gmail.com>
2025-12-19 19:21:48 -0500

    PreparedStatement

--- PreparedStatement/src/dev/lpa/Main.java
+++ PreparedStatement/src/dev/lpa/Main.java
@@ -2,6 +2,9 @@ package dev.lpa;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
@@ -18,6 +21,19 @@ public class Main {
             dataSource.setContinueBatchOnError(false);
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+
+        try (Connection connection = dataSource.getConnection(
+                System.getenv("MYSQL_USER"),
+                System.getenv("MYSQL_PASS"));
+        ) {
+            String sql = "SELECT * FROM music.albumview where artist_name = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, "Elf");
+            ResultSet resultSet = ps.executeQuery();
+            System.out.println(resultSet);
+        }catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
