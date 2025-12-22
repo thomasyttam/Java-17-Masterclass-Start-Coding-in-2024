19bd059bf2d053e5f2c6861b76d9081aa8274765
Thomas Tam <tyttam@gmail.com>
2025-12-21 22:44:51 -0500

    PreparedStatement

--- PreparedStatement/src/dev/lpa/Main.java
+++ PreparedStatement/src/dev/lpa/Main.java
@@ -9,6 +9,14 @@ import java.sql.SQLException;
 
 public class Main {
 
+    private static String ARTIST_INSERT =
+            "INSERT INTO music.artists (artist_name) VALUES (?)";
+    private static String ALBUM_INSERT =
+            "INSERT INTO music.albums (artist_id, album_name) VALUES (?, ?)";
+    private static String SONG_INSERT =
+            "INSERT INTO music.songs (album_id, track_number, song_title) " +
+                    "VALUES (?, ?, ?)";
+
     public static void main(String[] args) {
 
         var dataSource = new MysqlDataSource();
