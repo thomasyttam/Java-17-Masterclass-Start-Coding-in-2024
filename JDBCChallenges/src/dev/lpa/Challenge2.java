56f49111a7eb0e431c4ce852a579386664136fca
Thomas Tam <tyttam@gmail.com>
2025-12-27 20:15:41 -0500

    JDBCChallenges

--- JDBCChallenges/src/dev/lpa/Challenge2.java
+++ JDBCChallenges/src/dev/lpa/Challenge2.java
@@ -1,4 +1,30 @@
 package dev.lpa;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Challenge2 {
+
+        public static void main(String[] args) {
+
+            var dataSource = new MysqlDataSource();
+            dataSource.setServerName("localhost");
+            dataSource.setPort(3306);
+            dataSource.setUser(System.getenv("MYSQLUSER"));
+            dataSource.setPassword(System.getenv("MYSQLPASS"));
+
+            try (Connection conn = dataSource.getConnection()) {
+
+                String alterString =
+                        "ALTER TABLE storefront.order_details ADD COLUMN quantity INT";
+                Statement statement = conn.createStatement();
+                statement.execute(alterString);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
 }
