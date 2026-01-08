1b115a070c70ef3a7c79c6faeff71cf0ffe2a375
Thomas Tam <tyttam@gmail.com>
2026-01-08 18:15:42 -0500

    PreparedStatement

--- PreparedStatement/src/dev/lpa/MusicCallableStatement.java
+++ PreparedStatement/src/dev/lpa/MusicCallableStatement.java
@@ -1,4 +1,35 @@
 package dev.lpa;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class MusicCallableStatement {
+
+    private static final int ARTIST_COLUMN = 0;
+    private static final int ALBUM_COLUMN = 1;
+    private static final int SONG_COLUMN = 3;
+
+    public static void main(String[] args) {
+
+        Map<String, Map<String, String>> albums = null;
+
+        try (var lines = Files.lines(Path.of("NewAlbums.csv"))) {
+
+            albums = lines.map(s -> s.split(","))
+                    .collect(Collectors.groupingBy(s -> s[ARTIST_COLUMN],
+                            Collectors.groupingBy(s -> s[ALBUM_COLUMN],
+                                    Collectors.mapping(s -> s[SONG_COLUMN],
+                                            Collectors.joining(
+                                                    "\",\"",
+                                                    "[\"",
+                                                    "\"]"
+                                            )))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
