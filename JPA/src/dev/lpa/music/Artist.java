7fc6edc6f222509e05bb117af38318d2a32615df
Thomas Tam <tyttam@gmail.com>
2026-01-28 06:06:01 -0500

    JPA

--- JPA/src/dev/lpa/music/Artist.java
+++ JPA/src/dev/lpa/music/Artist.java
@@ -11,7 +11,36 @@ public class Artist {
 
     @Id
     @Column(name="artist_id")
+    private int artistId;
 
     @Column(name="artist_name")
     private String artistName;
+
+    public Artist() {
+    }
+
+    public Artist(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public Artist(int artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "artistName='" + artistName + '\'' +
+                ", artistId=" + artistId +
+                '}';
+    }
 }
