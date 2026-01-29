744218793235c4251646d19fcc1114bf236bea89
Thomas Tam <tyttam@gmail.com>
2026-01-29 05:50:33 -0500

    JPA

--- JPA/src/dev/lpa/Main.java
+++ JPA/src/dev/lpa/Main.java
@@ -1,8 +1,20 @@
 package dev.lpa;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        try (var sessionFactory =
+                     Persistence.createEntityManagerFactory(
+                             "dev.lpa.music");
+             EntityManager entityManager = sessionFactory.createEntityManager();
+        ) {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
