a04d0b944217d20c71ff943f091474840cecd349
Thomas Tam <tyttam@gmail.com>
2026-02-05 05:42:30 -0500

    JPA

--- JPA/src/dev/lpa/MainQuery.java
+++ JPA/src/dev/lpa/MainQuery.java
@@ -2,6 +2,8 @@ package dev.lpa;
 
 import dev.lpa.music.Artist;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 import java.util.List;
 
@@ -10,12 +12,26 @@ public class MainQuery {
     public static void main(String[] args) {
 
         List<Artist> artists = null;
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+                "dev.lpa.music");
+             EntityManager em = emf.createEntityManager();) {
+            var transaction = em.getTransaction();
+            transaction.begin();
+//            artists = getArtistsJPQL(em, "");
+            artists = getArtistsJPQL(em, "%Stev%");
+            artists.forEach(System.out::println);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static List<Artist> getArtistsJPQL(EntityManager em, String matchedValue) {
 
-        String jpql = "SELECT a FROM Artist a";
+//        String jpql = "SELECT a FROM Artist a";
+        String jpql = "SELECT a FROM Artist a WHERE a.artistName LIKE :partialName";
         var query = em.createQuery(jpql, Artist.class);
+        query.setParameter("partialName", matchedValue);
         return query.getResultList();
     }
 }
