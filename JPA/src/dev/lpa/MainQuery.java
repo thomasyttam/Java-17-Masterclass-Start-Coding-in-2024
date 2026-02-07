143008803fd2fa8605e4104c5fbe6c7380524c65
Thomas Tam <tyttam@gmail.com>
2026-02-07 07:59:43 -0500

    JPA

--- JPA/src/dev/lpa/MainQuery.java
+++ JPA/src/dev/lpa/MainQuery.java
@@ -4,6 +4,7 @@ import dev.lpa.music.Artist;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Tuple;
 
 import java.util.List;
 
@@ -20,6 +21,9 @@ public class MainQuery {
 //            artists = getArtistsJPQL(em, "");
             artists = getArtistsJPQL(em, "%Stev%");
             artists.forEach(System.out::println);
+
+            var names = getArtistNames(em, "Stev%");
+            names.forEach(System.out::println);
             transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -34,6 +38,18 @@ public class MainQuery {
 //        query.setParameter("partialName", matchedValue);
         String jpql = "SELECT a FROM Artist a WHERE a.artistName LIKE ?1";
         var query = em.createQuery(jpql, Artist.class);
+        query.setParameter(1, matchedValue);
+        return query.getResultList();
+    }
+
+//    private static List<String> getArtistNames(EntityManager em, String matchedValue) {
+    private static List<Tuple> getArtistNames(EntityManager em, String matchedValue) {
+
+//        String jpql = "SELECT a.artistId, a.artistName FROM Artist a WHERE a.artistName LIKE ?1";
+        String jpql = "SELECT a.artistId, a.artistName FROM Artist a " +
+                "WHERE a.artistName LIKE ?1";
+//        var query = em.createQuery(jpql, String.class);
+        var query = em.createQuery(jpql,Tuple.class);
         query.setParameter(1, matchedValue);
         return query.getResultList();
     }
