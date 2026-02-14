90068f31fa4693d5e578d96063385d8e776f49bf
Thomas Tam <tyttam@gmail.com>
2026-02-14 07:15:58 -0500

    JPA

---
+++ JPA/src/dev/lpa/music/Song.java
@@ -0,0 +1,21 @@
+package dev.lpa.music;
+
+public class Song {
+
+    private int songId;
+
+    private String songTitle;
+
+    private int trackNumber;
+
+    public Song() {
+    }
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+}
