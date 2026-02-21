6721c3ffcaa0c963cfbdfff7342b916c0bd29d11
Thomas Tam <tyttam@gmail.com>
2026-02-21 07:32:19 -0500

    ClientServer

--- ClientServer/src/dev/lpa/server/SimpleServer.java
+++ ClientServer/src/dev/lpa/server/SimpleServer.java
@@ -1,8 +1,16 @@
 package dev.lpa.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class SimpleServer {
 
     public static void main(String[] args) {
-        
+
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+
+        } catch (IOException e) {
+            System.out.println("Server exception " + e.getMessage());
+        }
     }
 }
