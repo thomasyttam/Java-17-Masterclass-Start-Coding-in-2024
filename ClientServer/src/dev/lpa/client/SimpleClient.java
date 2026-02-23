af0001fa9e6ec46f4591775e6415fa19f3a9783c
Thomas Tam <tyttam@gmail.com>
2026-02-23 05:52:58 -0500

    ClientServer

--- ClientServer/src/dev/lpa/client/SimpleClient.java
+++ ClientServer/src/dev/lpa/client/SimpleClient.java
@@ -1,8 +1,27 @@
 package dev.lpa.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class SimpleClient {
 
     public static void main(String[] args) {
-        
+
+        try (Socket socket = new Socket("localhost", 5000)) {
+
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter output =
+                    new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner scanner = new Scanner(System.in);
+            
+        } catch (IOException e) {
+            System.out.println("Client error: " + e.getMessage());
+        }
     }
 }
