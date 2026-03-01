4b684e9402a6b863489b7148cac550988e421d1a
Thomas Tam <tyttam@gmail.com>
2026-03-01 10:08:00 -0500

    ClientServer

--- ClientServer/src/dev/lpa/server/SimpleServerChannel.java
+++ ClientServer/src/dev/lpa/server/SimpleServerChannel.java
@@ -1,7 +1,9 @@
 package dev.lpa.server;
 
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
 public class SimpleServerChannel {
 
@@ -12,6 +14,31 @@ public class SimpleServerChannel {
 
             System.out.println("Server is listening on port " +
                     serverChannel.socket().getLocalPort());
+
+            while (true) {
+
+                SocketChannel clientChannel = serverChannel.accept();
+                System.out.printf("Client %s connected%n",
+                        clientChannel.socket().getRemoteSocketAddress());
+
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                SocketChannel channel = clientChannel;
+                int readBytes = channel.read(buffer);
+
+                if (readBytes > 0) {
+                    buffer.flip(); // flip the buffer state from writeable to readble
+
+                    channel.write(ByteBuffer.wrap("Echo from server: ".getBytes()));
+                    while (buffer.hasRemaining()) {
+                        channel.write(buffer);
+                    }
+                    buffer.clear();
+                } else if (readBytes == -1) {
+                    System.out.printf("Connection to %s lost%n",
+                            channel.socket().getRemoteSocketAddress());
+                    channel.close();
+                }
+            }
         } catch (Exception e) {
             System.out.println("Server exception " + e.getMessage());
         }
