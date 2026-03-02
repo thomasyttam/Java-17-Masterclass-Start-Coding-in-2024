f5900a3766c6d6854df18dd57f3d4d7955ff7343
Thomas Tam <tyttam@gmail.com>
2026-03-02 06:17:52 -0500

    ClientServer

---
+++ ClientServer/src/dev/lpa/Main.java
@@ -0,0 +1,15 @@
+package dev.lpa;
+
+import java.nio.ByteBuffer;
+import java.util.function.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void doOperation(String op, ByteBuffer buffer, Consumer<ByteBuffer> c) {
+        
+    }
+}
