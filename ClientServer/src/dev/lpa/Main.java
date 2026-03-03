f51f39a5aa8974853bbff31b9e837549d30d7e41
Thomas Tam <tyttam@gmail.com>
2026-03-03 05:48:16 -0500

    ClientServer

--- ClientServer/src/dev/lpa/Main.java
+++ ClientServer/src/dev/lpa/Main.java
@@ -10,6 +10,12 @@ public class Main {
     }
 
     private static void doOperation(String op, ByteBuffer buffer, Consumer<ByteBuffer> c) {
-        
+        System.out.printf("%-30s", op);
+        c.accept(buffer);
+        System.out.printf("Capacity = %d, Limit = %d, Position = %d, Remaining = %d%n",
+                buffer.capacity(),
+                buffer.limit(),
+                buffer.position(),
+                buffer.remaining());
     }
 }
