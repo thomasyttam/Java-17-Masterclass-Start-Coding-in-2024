e59014370c64494e5cb528d0efa66b7d1c8d4e12
Thomas Tam <tyttam@gmail.com>
2026-03-14 09:30:06 -0400

    ClientServer

--- ClientServer/src/dev/lpa/server/UDPPacketServer.java
+++ ClientServer/src/dev/lpa/server/UDPPacketServer.java
@@ -1,6 +1,7 @@
 package dev.lpa.server;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class UDPPacketServer {
@@ -11,6 +12,14 @@ public class UDPPacketServer {
     public static void main(String[] args) {
 
         try (DatagramSocket serverSocket = new DatagramSocket(PORT)) {
+
+            byte[] buffer = new byte[PACKET_SIZE];
+
+            System.out.println("Waiting for client to connect...");
+            DatagramPacket clientPacket = new DatagramPacket(buffer, buffer.length);
+            serverSocket.receive(clientPacket);
+            String audioFileName = new String(buffer, 0, clientPacket.getLength());
+            System.out.println("Client requested to listen to: " + audioFileName);
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
