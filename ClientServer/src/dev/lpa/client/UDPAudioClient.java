fbcbab6757dd5da111257ec86f3c8feb81ebf15c
Thomas Tam <tyttam@gmail.com>
2026-03-18 06:14:10 -0400

    ClientServer

--- ClientServer/src/dev/lpa/client/UDPAudioClient.java
+++ ClientServer/src/dev/lpa/client/UDPAudioClient.java
@@ -1,9 +1,11 @@
 package dev.lpa.client;
 
+import javax.sound.sampled.*;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 
 public class UDPAudioClient {
 
@@ -18,8 +20,35 @@ public class UDPAudioClient {
             DatagramPacket packet1 = new DatagramPacket(audioFileName,
                     audioFileName.length, InetAddress.getLocalHost(), SERVER_PORT);
             clientSocket.send(packet1);
-        } catch (IOException e) {
+            playStreamedAudio(clientSocket);
+        } catch (IOException | LineUnavailableException e) {
             System.out.println(e.getMessage());
         }
     }
+
+    private static void playStreamedAudio(DatagramSocket clientSocket)
+            throws SocketException, LineUnavailableException {
+
+        clientSocket.setSoTimeout(2000);
+        AudioFormat format = new AudioFormat( 22050, 16,
+                1, true, false);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+        SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+        line.open();
+        line.start();
+
+        byte[] buffer = new byte[PACKET_SIZE];
+        while (true) {
+            try {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                clientSocket.receive(packet);
+                line.write(buffer, 0, packet.getLength());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                break;
+            }
+        }
+        line.close();
+    }
+
 }
