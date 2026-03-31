30874d5e71b08df0932fea65ca9ac48ebec45c64
Thomas Tam <tyttam@gmail.com>
2026-03-31 06:25:34 -0400

    HttpProject

--- HttpProject/src/dev/lpa/HttpExample.java
+++ HttpProject/src/dev/lpa/HttpExample.java
@@ -14,7 +14,8 @@ public class HttpExample {
     public static void main(String[] args) {
 
         try {
-            URL url = new URL("http://example.com");
+//            URL url = new URL("http://example.com");
+            URL url = new URL("http://example.com/extra");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("User-Agent","Chrome");
@@ -25,6 +26,7 @@ public class HttpExample {
             System.out.printf("Response code: %d%n",responseCode);
             if (responseCode != HTTP_OK) {
                 System.out.println("Error reading web page " + url);
+                System.out.printf("Error: %s%n",connection.getResponseMessage());
                 return;
             }
             printContents(connection.getInputStream());
