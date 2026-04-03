c0f4374b8caefbe3360092ab55f9f693744871b5
Thomas Tam <tyttam@gmail.com>
2026-04-03 09:24:20 -0400

    HttpProject

--- HttpProject/src/dev/lpa/server/SimpleHttpServer.java
+++ HttpProject/src/dev/lpa/server/SimpleHttpServer.java
@@ -5,7 +5,11 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+import static java.net.HttpURLConnection.HTTP_OK;
+
 public class SimpleHttpServer {
+
+    private static long visitorCounter = 0;
 
     public static void main(String[] args) {
 
@@ -17,15 +21,27 @@ public class SimpleHttpServer {
                 String requestMethod = exchange.getRequestMethod();
                 System.out.println("Request Method: " + requestMethod);
 
+                if (requestMethod.equals("POST")) {
+                    visitorCounter++;
+                }
+
                 String response = """
                 <html>
                     <body>
                         <h1>Hello World from My Http Server</h1>
+                        <p>Number of Visitors who signed up = %d<p>
+                        <form method="post">
+                            <input type="submit" value="Submit">
+                        </form>
                     </body>
                 </html>
-                """;
+                """.formatted(visitorCounter);
 
                 var bytes = response.getBytes();
+                exchange.sendResponseHeaders(HTTP_OK, bytes.length);
+                exchange.getResponseBody().write(bytes);
+                exchange.close();
+
             });
 
 
