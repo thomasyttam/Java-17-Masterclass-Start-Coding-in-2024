bde36b6588ccb20f8c78247d9d22892ad367d778
Thomas Tam <tyttam@gmail.com>
2026-04-13 06:22:27 -0400

    HttpProject

--- HttpProject/src/dev/lpa/HttpClientPost.java
+++ HttpProject/src/dev/lpa/HttpClientPost.java
@@ -1,8 +1,7 @@
 package dev.lpa;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -16,16 +15,18 @@ public class HttpClientPost {
     public static void main(String[] args) {
 
         try {
-            URL url = new URL("http://localhost:8080");
             HttpClient client = HttpClient.newBuilder()
                     .connectTimeout(Duration.ofMinutes(1))
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .build();
 
             HttpRequest request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(url.toURI())
-                    .header("User-Agent", "Chrome")
-                    .headers("Accept", "application/json", "Accept","text/html")
-                    .timeout(Duration.ofSeconds(30))
+                    .POST(HttpRequest.BodyPublishers.ofString(
+                            "first=Joe&last=Smith"
+                    ))
+                    .uri(URI.create("http://localhost:8080"))
+                    .header("Content-Type",
+                            "application/x-www-form-urlencoded")
                     .build();
 
 //            HttpResponse<String> response = client.send(request,
@@ -33,7 +34,7 @@ public class HttpClientPost {
             HttpResponse<Stream<String>> response = client.send(request,
                     HttpResponse.BodyHandlers.ofLines());
             if (response.statusCode() != HTTP_OK) {
-                System.out.println("Error reading web page " + url);
+                System.out.println("Error reading web page " + request.uri());
                 return;
             }
 //            System.out.println( response.body());
@@ -41,7 +42,7 @@ public class HttpClientPost {
                     .filter(s -> s.contains("<h1>"))
                     .map(s -> s.replaceAll("<[^>]*>", "").strip())
                     .forEach(System.out::println);
-        } catch (IOException | URISyntaxException | InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
