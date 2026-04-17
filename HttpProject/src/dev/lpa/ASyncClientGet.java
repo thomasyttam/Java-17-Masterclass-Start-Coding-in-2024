8d408172d2aa602ee1f895b5a91445007640ac3b
Thomas Tam <tyttam@gmail.com>
2026-04-17 06:22:28 -0400

    HttpProject

--- HttpProject/src/dev/lpa/ASyncClientGet.java
+++ HttpProject/src/dev/lpa/ASyncClientGet.java
@@ -7,6 +7,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
@@ -28,15 +29,24 @@ public class ASyncClientGet {
                     .timeout(Duration.ofSeconds(30))
                     .build();
 
-            HttpResponse<Stream<String>> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofLines());
+//            HttpResponse<Stream<String>> response = client.send(request,
+//                    HttpResponse.BodyHandlers.ofLines());
+            HttpResponse<Stream<String>> response;
+            CompletableFuture<HttpResponse<Stream<String>>> responseFuture =
+                    client.sendAsync(request, HttpResponse.BodyHandlers.ofLines());
 
-            int result = -1;
-            while((result = response.statusCode()) == -1) {
+            while(!responseFuture.isDone()) {
                 System.out.print(". ");
                 TimeUnit.SECONDS.sleep(1);
             }
             System.out.println();
+//            try {
+//                response = responseFuture.get();
+//            } catch (ExecutionException e) {
+//                throw new RuntimeException(e);
+//            }
+
+            response = responseFuture.join();
             handleResponse(response);
 
         } catch (IOException | URISyntaxException | InterruptedException e) {
