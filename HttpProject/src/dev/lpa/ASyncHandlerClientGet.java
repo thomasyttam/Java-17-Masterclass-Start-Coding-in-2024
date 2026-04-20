f2f0d958702f8c513fc7f1e86faeaf0835124e85
Thomas Tam <tyttam@gmail.com>
2026-04-20 06:06:14 -0400

    HttpProject

--- HttpProject/src/dev/lpa/ASyncHandlerClientGet.java
+++ HttpProject/src/dev/lpa/ASyncHandlerClientGet.java
@@ -8,9 +8,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.Stream;
 
 import static java.net.HttpURLConnection.HTTP_OK;
@@ -30,10 +28,21 @@ public class ASyncHandlerClientGet {
                     .headers("Accept", "application/json", "Accept", "text/html")
                     .timeout(Duration.ofSeconds(30))
                     .build();
-            
+
             HttpResponse<Stream<String>> response;
             CompletableFuture<HttpResponse<Stream<String>>> responseFuture =
                     client.sendAsync(request, HttpResponse.BodyHandlers.ofLines());
+
+//            responseFuture.thenAccept(r -> handleResponse(r));
+            responseFuture.thenAccept(ASyncHandlerClientGet::handleResponse);
+            
+            System.out.println(
+                    "Ten Jobs to do besides handling the response.");
+            int jobs = 0;
+            while (jobs++ < 10) {
+                TimeUnit.SECONDS.sleep(1);
+                System.out.printf("Job %d ", jobs);
+            }
 
         } catch (IOException | URISyntaxException | InterruptedException e) {
             throw new RuntimeException(e);
