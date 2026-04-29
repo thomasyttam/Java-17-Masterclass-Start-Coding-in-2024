5e3701973db8b793f1ade311d18c20370f64325a
Thomas Tam <tyttam@gmail.com>
2026-04-29 05:57:28 -0400

    HttpProject

--- HttpProject/src/dev/lpa/ConcurrentRequests.java
+++ HttpProject/src/dev/lpa/ConcurrentRequests.java
@@ -1,15 +1,20 @@
 package dev.lpa;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class ConcurrentRequests {
+
+    private static final Path orderTracking = Path.of("orderTracking.json");
 
     public static void main(String[] args) {
 
@@ -31,13 +36,40 @@ public class ConcurrentRequests {
 
         HttpClient client = HttpClient.newHttpClient();
         sendGets(client, sites);
+
+        if (!Files.exists(orderTracking)) {
+            try {
+                Files.createFile(orderTracking);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     private static void sendGets(HttpClient client, List<URI> uris) {
 
-
-
         var futures = uris.stream()
+                .map(uri -> HttpRequest.newBuilder(uri))
+                .map(HttpRequest.Builder::build)
+                .map(request -> client.sendAsync(
+                        request, HttpResponse.BodyHandlers.ofString()))
+                .toList();
+
+        var allFutureRequests = CompletableFuture.allOf(
+                futures.toArray(new CompletableFuture<?>[0])
+        );
+
+        allFutureRequests.join();
+
+        futures.forEach(f -> {
+            System.out.println(f.join().body());
+        });
+    }
+
+    private static void sendPosts(HttpClient client, String baseURI,
+                                  String paramString, Map<String,Integer> orders) {
+
+        var futures = orders.entrySet().stream()
                 .map(uri -> HttpRequest.newBuilder(uri))
                 .map(HttpRequest.Builder::build)
                 .map(request -> client.sendAsync(
