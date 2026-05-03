94fde978bcb673eb1c55dfbb2bd40779849839ba
Thomas Tam <tyttam@gmail.com>
2026-05-03 09:25:36 -0400

    HttpProject

--- HttpProject/src/dev/lpa/handlers/ThreadSafeFileHandler.java
+++ HttpProject/src/dev/lpa/handlers/ThreadSafeFileHandler.java
@@ -1,7 +1,11 @@
 package dev.lpa.handlers;
 
+import java.io.IOException;
 import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -16,6 +20,19 @@ public class ThreadSafeFileHandler implements HttpResponse.BodyHandler<Path> {
 
     @Override
     public HttpResponse.BodySubscriber<Path> apply(HttpResponse.ResponseInfo responseInfo) {
-        return null;
+        return HttpResponse.BodySubscribers.mapping(
+                HttpResponse.BodySubscribers.ofString(
+                        Charset.defaultCharset()
+                ), value -> {
+                    lock.lock();
+                    try {
+                        Files.writeString(path, value + "\r", StandardOpenOption.APPEND);
+                        return path;
+                    } catch (IOException e) {
+                        throw new RuntimeException("Failed to write response", e);
+                    } finally {
+                        lock.unlock();
+                    }
+                });
     }
 }
