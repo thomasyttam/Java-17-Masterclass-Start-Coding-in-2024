db1fcedf849caa5b80ead715510496ebd734f19b
Thomas Tam <tyttam@gmail.com>
2026-05-11 06:04:56 -0400

    WebSocket

--- WebSocket/src/dev/lpa/WebSocketClient.java
+++ WebSocket/src/dev/lpa/WebSocketClient.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
 import java.util.Scanner;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class WebSocketClient {
@@ -21,7 +22,14 @@ public class WebSocketClient {
                 .buildAsync(new URI("ws://localhost:8080?name=%s"
                                 .formatted(name)),
                         new WebSocket.Listener() {
-
+                            @Override
+                            public CompletionStage<?> onText(WebSocket webSocket,
+                                                             CharSequence data,
+                                                             boolean last) {
+                                System.out.println(data);
+                                return WebSocket.Listener.super.onText(webSocket,
+                                        data, last);
+                            }
                         }).join();
 
         while (true) {
