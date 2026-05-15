afa242ebb833b04c8b12a2d24738c4f7500da19c
Thomas Tam <tyttam@gmail.com>
2026-05-15 05:51:49 -0400

    DebuggerDemo

--- DebuggerDemo/src/com/timbuchalka/StringUtilities.java
+++ DebuggerDemo/src/com/timbuchalka/StringUtilities.java
@@ -5,11 +5,20 @@ package com.timbuchalka;
  */
 public class StringUtilities {
     private StringBuilder sBuilder = new StringBuilder();
-    private int charsAdded = 0;
+    private int charsAdded = 0; // add watch point when modify
 
     public void addChar(StringBuilder sBuilder, char c) {
 //        this.sBuilder.append(c); // append char on the instance variable (this)
         sBuilder.append(c); // break point set
         charsAdded++;
+    }
+
+    public String upperAndPrefix(String str) {
+        String upper = str.toUpperCase();
+        return "Prefix_" + upper;
+    }
+
+    public String addSuffix(String str) {
+        return str + "__Suffix";
     }
 }
