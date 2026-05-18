8777f64671faeac436ca225f477af03e2e152e56
Thomas Tam <tyttam@gmail.com>
2026-05-18 18:29:00 -0400

    Banking

--- Banking/src/com/timbuchalka/BankAccountTest.java
+++ Banking/src/com/timbuchalka/BankAccountTest.java
@@ -1,5 +1,6 @@
 package com.timbuchalka;
 
+import static junit.framework.TestCase.fail;
 /**
  *
  */
@@ -7,14 +8,17 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
+        fail("This test has yet to be implemented");
     }
 
     @org.junit.Test
     public void withdraw() {
+        fail("This test has yet to be implemented");
     }
 
     @org.junit.Test
     public void getBalance() {
+        fail("This test has yet to be implemented");
     }
 
 }
