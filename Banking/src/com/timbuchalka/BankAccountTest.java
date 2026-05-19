66391b1ba8dd87af979220928a8083f8590ed001
Thomas Tam <tyttam@gmail.com>
2026-05-19 03:05:18 -0400

    Banking

--- Banking/src/com/timbuchalka/BankAccountTest.java
+++ Banking/src/com/timbuchalka/BankAccountTest.java
@@ -1,5 +1,6 @@
 package com.timbuchalka;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.fail;
 /**
  *
@@ -8,7 +9,10 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
-        fail("This test has yet to be implemented");
+//        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0)
     }
 
     @org.junit.Test
@@ -21,4 +25,8 @@ public class BankAccountTest {
         fail("This test has yet to be implemented");
     }
 
+//    @org.junit.Test
+//    public void dummyTest() {
+//        assertEquals(20, 21);
+//    }
 }
