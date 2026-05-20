257d52198730c08086f1ad0a7a94225cb0864908
Thomas Tam <tyttam@gmail.com>
2026-05-20 04:16:38 -0400

    Banking

--- Banking/src/com/timbuchalka/BankAccountTest.java
+++ Banking/src/com/timbuchalka/BankAccountTest.java
@@ -12,7 +12,8 @@ public class BankAccountTest {
 //        fail("This test has yet to be implemented");
         BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
         double balance = account.deposit(200.00, true);
-        assertEquals(1200.00, balance, 0)
+        assertEquals(1200.00, balance, 0);
+        assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
@@ -21,8 +22,19 @@ public class BankAccountTest {
     }
 
     @org.junit.Test
-    public void getBalance() {
-        fail("This test has yet to be implemented");
+    public void getBalance_deposit() {
+//        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void getBalance_withdraw() {
+//        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        account.withdraw(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
     }
 
 //    @org.junit.Test
