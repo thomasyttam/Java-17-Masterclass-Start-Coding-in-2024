2943d69769cb93ab5a9967e387a547919247bb47
Thomas Tam <tyttam@gmail.com>
2026-05-28 05:00:48 -0400

    Banking

--- Banking/src/com/timbuchalka/BankAccountTestParameterized.java
+++ Banking/src/com/timbuchalka/BankAccountTestParameterized.java
@@ -1,8 +1,15 @@
 package com.timbuchalka;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  *
  */
+@RunWith(Parameterized.class)
 public class BankAccountTestParameterized {
 
     private BankAccount account;
@@ -11,5 +18,16 @@ public class BankAccountTestParameterized {
     public void setup() {
         account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         System.out.println("Running a test...");
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testConditions() {
+        return Arrays.asList(new Object[][] {
+                {100.00, true, 1100.00},
+                {200.00, true, 1200.00},
+                {325.14, true, 1325.14},
+                {489.33, true, 1489.33},
+                {1000.00, true, 2000.00}
+        });
     }
 }
