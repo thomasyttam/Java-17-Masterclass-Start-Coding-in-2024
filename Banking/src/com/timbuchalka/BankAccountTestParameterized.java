88b8c3a896dc62947120f7611f2515949abd5432
Thomas Tam <tyttam@gmail.com>
2026-05-29 05:44:40 -0400

    Banking

--- Banking/src/com/timbuchalka/BankAccountTestParameterized.java
+++ Banking/src/com/timbuchalka/BankAccountTestParameterized.java
@@ -6,13 +6,24 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  *
  */
-@RunWith(Parameterized.class)
+@RunWith(Parameterized.class) // tell IntelliJ it is not normal test clas, it runs parameterise test
 public class BankAccountTestParameterized {
 
     private BankAccount account;
+    private double amount;
+    private boolean branch;
+    private double expected;
+
+    public BankAccountTestParameterized(double amount, boolean branch, double expected) {
+        this.amount = amount;
+        this.branch = branch;
+        this.expected = expected;
+    }
 
     @org.junit.Before
     public void setup() {
@@ -20,7 +31,7 @@ public class BankAccountTestParameterized {
         System.out.println("Running a test...");
     }
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters // tell J unit that these are the parameters we want to use.
     public static Collection<Object[]> testConditions() {
         return Arrays.asList(new Object[][] {
                 {100.00, true, 1100.00},
@@ -29,5 +40,11 @@ public class BankAccountTestParameterized {
                 {489.33, true, 1489.33},
                 {1000.00, true, 2000.00}
         });
+    }
+
+    @org.junit.Test
+    public void deposit() throws Exception {
+        account.deposit(amount, branch);
+        assertEquals(expected, account.getBalance(), 0.1);
     }
 }
