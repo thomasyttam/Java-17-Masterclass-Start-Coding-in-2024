8430bdab3350b99c081a532ed9eceb71f7e73e6c
Thomas Tam <tyttam@gmail.com>
2026-05-31 09:10:39 -0400

    JunitChallenge

--- JunitChallenge/src/com/timbuchalka/Utilities.java
+++ JunitChallenge/src/com/timbuchalka/Utilities.java
@@ -23,4 +23,28 @@ public class Utilities {
 
         return result;
     }
+
+    // Removes pairs of the same character that are next
+    // to each other, by removing on e occurrencd of the character.
+    // "ABBCDEEF" -> "ABCDEF"
+    // "ABCBDEEF" -> "ABCBDEF" (the two B's aren't next to each other, so they
+    // aren't removed.
+    public String removePairs(String source) {
+
+        // If length is less than  2, there won't be any pairs
+        if (source.length() < 2) {
+            return source;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        char[] string = source.toCharArray();
+
+        for(int i=0; i < string.length - 1; i++) {
+            System.out.println(string[i]);
+            if(string[i] != string[i + 1]) {
+                sb.append(string[i]);
+            }
+        }
+        return sb.toString();
+    }
 }
