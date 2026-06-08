63152a2a9f17672e541298a0517bb62e9ae9aa3e
Thomas Tam <tyttam@gmail.com>
2026-06-08 03:42:29 -0400

    HelloWorldFX

--- HelloWorldFX/src/main/java/com/example/helloworldfx/HelloApplication.java
+++ HelloWorldFX/src/main/java/com/example/helloworldfx/HelloApplication.java
@@ -2,15 +2,28 @@ package com.example.helloworldfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+        
+        Label greeting = new Label("Welcome to JavaFX!");
+        root.getChildren().add(greeting);
+
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello JavaFX!");
         stage.setScene(scene);
         stage.show();
     }
