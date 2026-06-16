7ca640aef299538be950422bebbf3c38c1e481c4
Thomas Tam <tyttam@gmail.com>
2026-06-16 03:09:57 -0400

    Layout

--- Layout/src/main/java/com/example/layout/HelloApplication.java
+++ Layout/src/main/java/com/example/layout/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 240);
         stage.setTitle("Hello World!");
         stage.setScene(scene);
         stage.show();
