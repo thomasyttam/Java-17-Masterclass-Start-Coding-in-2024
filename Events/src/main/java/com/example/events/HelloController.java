1ffa60c243f250e5ed79a933826e436cefddcc76
Thomas Tam <tyttam@gmail.com>
2026-07-05 08:31:51 -0400

    Events

--- Events/src/main/java/com/example/events/HelloController.java
+++ Events/src/main/java/com/example/events/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.events;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+//    @FXML
+//    private Label welcomeText;
+//
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
+    public void onButtonClick() {
+        System.out.println("Hello");
     }
 }
