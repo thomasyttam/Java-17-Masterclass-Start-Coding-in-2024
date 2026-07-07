2720887fed7986fd0ce2b362128545ec6ac3cd70
Thomas Tam <tyttam@gmail.com>
2026-07-07 01:17:50 -0400

    Events

--- Events/src/main/java/com/example/events/HelloController.java
+++ Events/src/main/java/com/example/events/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.events;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
@@ -14,7 +15,13 @@ public class HelloController {
     @FXML
     private TextField nameField;
 
-    public void onButtonClick() {
+//    @FXML
+//    public void onButtonClick() {
+//        System.out.println("Hello, " + nameField.getText());
+//    }
+
+    @FXML
+    public void onButtonClick(ActionEvent e) {
         System.out.println("Hello, " + nameField.getText());
     }
 }
