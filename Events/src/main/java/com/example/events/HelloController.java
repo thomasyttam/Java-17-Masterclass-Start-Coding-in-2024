59dc190c73698ff9ee918b125173efd5bbe0d48f
Thomas Tam <tyttam@gmail.com>
2026-07-08 01:04:28 -0400

    Events

--- Events/src/main/java/com/example/events/HelloController.java
+++ Events/src/main/java/com/example/events/HelloController.java
@@ -2,6 +2,7 @@ package com.example.events;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class HelloController {
@@ -14,14 +15,37 @@ public class HelloController {
 //    }
     @FXML
     private TextField nameField;
-
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
 //    @FXML
 //    public void onButtonClick() {
 //        System.out.println("Hello, " + nameField.getText());
 //    }
 
     @FXML
-    public void onButtonClick(ActionEvent e) {
-        System.out.println("Hello, " + nameField.getText());
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+//        System.out.println("Hello, " + nameField.getText());
+//        System.out.println("The following button was pressed: " +e.getSource());
+        if(e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if(e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 }
