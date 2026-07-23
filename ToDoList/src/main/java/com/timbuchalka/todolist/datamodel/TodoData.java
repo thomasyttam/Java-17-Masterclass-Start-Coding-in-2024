6ebbb547915137e1db1f7c0a5ef6d5e5d5a0cc8c
Thomas Tam <tyttam@gmail.com>
2026-07-23 00:56:00 -0400

    ToDoList

---
+++ ToDoList/src/main/java/com/timbuchalka/todolist/datamodel/TodoData.java
@@ -0,0 +1,28 @@
+package com.timbuchalka.todolist.datamodel;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class TodoData {
+    private static TodoData instance = new TodoData();
+    private static String filename = "TodoListItems.txt";
+
+    private List<TodoItem> todoItems;
+    private DateTimeFormatter formatter;
+
+    public static TodoData getInstance() {
+        return instance;
+    }
+
+    private TodoData() {
+        formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    }
+
+    public List<TodoItem> getTodoItems() {
+        return todoItems;
+    }
+
+    public void setTodoItems(List<TodoItem> todoItems) {
+        this.todoItems = todoItems;
+    }
+}
