f8ead728c0416b0d31904f766f4ff763b90c2916
Thomas Tam <tyttam@gmail.com>
2026-08-15 06:54:59 -0400

    ToDoList

--- ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
+++ ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
@@ -8,9 +8,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.util.Callback;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
@@ -79,8 +81,16 @@ public class Controller {
             public ListCell<TodoItem> call(ListView<TodoItem> param) {
                 ListCell<TodoItem> cell = new ListCell<TodoItem>() {
                     @Override
-                    protected void updateItem(TodoItem todoItem, boolean empty) {
-                        super.updateItem(todoItem, empty);
+                    protected void updateItem(TodoItem item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if(empty) {
+                            setText(null);
+                        } else {
+                            setText(item.getShortDescription());
+                            if(item.getDeadline().equals(LocalDate.now())){
+                                setTextFill(Color.RED);
+                            }
+                        }
                     }
                 };
                 return cell;
