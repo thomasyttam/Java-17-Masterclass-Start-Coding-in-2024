5d2fc08833fe247cab4b28d5fcfde4619e088571
Thomas Tam <tyttam@gmail.com>
2026-08-16 07:18:33 -0400

    ToDoList

--- ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
+++ ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
@@ -89,6 +89,8 @@ public class Controller {
                             setText(item.getShortDescription());
                             if(item.getDeadline().equals(LocalDate.now())){
                                 setTextFill(Color.RED);
+                            } else if (item.getDeadline().equals(LocalDate.now().plusDays(1))) {
+                                setTextFill(Color.YELLOW);
                             }
                         }
                     }
