67cbdf68aa5394b901eb9aa5ff65dfa21a9b9b16
Thomas Tam <tyttam@gmail.com>
2026-08-29 09:53:15 -0400

    ToDoList

--- ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
+++ ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
@@ -9,6 +9,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
@@ -162,7 +163,12 @@ public class Controller {
 
     @FXML
     public void handleKeyPressed(KeyEvent keyEvent) {
-
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+        if(selectedItem != null) {
+            if(keyEvent.getCode().equals(KeyCode.DELETE)) {
+                deleteItem(selectedItem);
+            }
+        }
     }
 
     @FXML
