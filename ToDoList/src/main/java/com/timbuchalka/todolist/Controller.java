21aad0adf81774f50f57508d1ed8ade26204a8e7
Thomas Tam <tyttam@gmail.com>
2026-09-01 05:43:51 -0400

    ToDoList

--- ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
+++ ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
@@ -4,6 +4,7 @@ import com.timbuchalka.todolist.datamodel.TodoData;
 import com.timbuchalka.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -18,6 +19,7 @@ import javafx.util.Callback;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -88,6 +90,15 @@ public class Controller {
 
 //        todoListView.getItems().setAll(todoItems);
 //        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+
+        SortedList<TodoItem> sortedList = new SortedList<TodoItem>(TodoData.getInstance(),
+                new Comparator<TodoItem>() {
+                    @Override
+                    public int compare(TodoItem o1, TodoItem o2) {
+                        return o1.getDeadline().compareTo(o2.getDeadline());
+                    }
+                });
+
         todoListView.setItems(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
