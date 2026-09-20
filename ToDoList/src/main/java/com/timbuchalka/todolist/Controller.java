a22dc1d8cdd7fad89215dc04392efd05ba88a73f
Thomas Tam <tyttam@gmail.com>
2026-09-20 07:36:46 -0400

    ToDoList

--- ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
+++ ToDoList/src/main/java/com/timbuchalka/todolist/Controller.java
@@ -2,6 +2,7 @@ package com.timbuchalka.todolist;
 
 import com.timbuchalka.todolist.datamodel.TodoData;
 import com.timbuchalka.todolist.datamodel.TodoItem;
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.transformation.FilteredList;
@@ -278,6 +279,6 @@ public class Controller {
 
     @FXML
     public void handleExit() {
-
+        Platform.exit();;
     }
 }
